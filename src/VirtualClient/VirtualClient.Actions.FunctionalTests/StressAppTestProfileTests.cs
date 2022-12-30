1e8f10a99535f3bc99718d49a0be68bcb38ed925
Aditya Abhishek <adityaa@microsoft.com>
2022-12-30 16:12:43 +0530

    onboarding stressAppTest

---
+++ src/VirtualClient/VirtualClient.Actions.FunctionalTests/StressAppTestProfileTests.cs
@@ -0,0 +1,88 @@
+﻿namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using VirtualClient.Common;
+    using VirtualClient.Contracts;
+
+    [TestFixture]
+    [Category("Functional")]
+    public class StressAppTestProfileTests
+    {
+        private DependencyFixture fixture;
+
+        [OneTimeSetUp]
+        public void SetupFixture()
+        {
+            this.fixture = new DependencyFixture();
+            ComponentTypeCache.Instance.LoadComponentTypes(TestDependencies.TestDirectory);
+        }
+
+        [Test]
+        [TestCase("PERF-MEM-STRESSAPPTEST.json", PlatformID.Unix)]
+        public void StressAppTestWorkloadProfileParametersAreInlinedCorrectly(string profile, PlatformID platform)
+        {
+            this.fixture.Setup(platform);
+            using (ProfileExecutor executor = TestDependencies.CreateProfileExecutor(profile, this.fixture.Dependencies))
+            {
+                WorkloadAssert.ParameterReferencesInlined(executor.Profile);
+            }
+        }
+
+        [Test]
+        [TestCase("PERF-MEM-STRESSAPPTEST.json", PlatformID.Unix)]
+        public async Task StressAppTestWorkloadProfileExecutesTheExpectedWorkloads(string profile, PlatformID platform)
+        {
+            this.SetupDefaultMockBehaviors(platform);
+            IEnumerable<string> expectedCommands = this.GetProfileExpectedCommands(platform);            
+            
+            // Setup the expectations for the workload
+            // - Workload package is installed and exists.
+            // - Workload binaries/executables exist on the file system.
+            // - The workload generates valid results.
+
+            this.fixture.ProcessManager.OnCreateProcess = (command, arguments, workingDir) =>
+            {
+                IProcessProxy process = this.fixture.CreateProcess(command, arguments, workingDir);
+                return process;
+            };
+
+            using (ProfileExecutor executor = TestDependencies.CreateProfileExecutor(profile, this.fixture.Dependencies))
+            {
+                await executor.ExecuteAsync(ProfileTiming.OneIteration(), CancellationToken.None)
+                    .ConfigureAwait(false);
+
+                WorkloadAssert.CommandsExecuted(this.fixture, expectedCommands.ToArray());
+            }
+        }        
+
+        private IEnumerable<string> GetProfileExpectedCommands(PlatformID platform)
+        {
+            List<string> commands = null;
+            if (platform == PlatformID.Unix)
+            {
+                commands = new List<string>
+                {
+                    $"sudo chmod +x \"{this.fixture.GetPackagePath()}/stressapptest/linux-x64/stressapptest\"",
+                    @$"{this.fixture.GetPackagePath()}/stressapptest/linux-x64/stressapptest -s 60 -l stressapptestLogs_1.txt"
+                };
+            }
+
+            return commands;
+        }
+
+        private void SetupDefaultMockBehaviors(PlatformID platform)
+        {
+            this.fixture.Setup(platform);
+            if (platform == PlatformID.Unix)
+            {
+                this.fixture.SetupWorkloadPackage("stressapptest", expectedFiles: @"linux-x64/stressapptest");
+                this.fixture.SetupFile("stressapptest", @"linux-x64/stressapptestLogs_1.txt", TestDependencies.GetResourceFileContents("Results_StressAppTest.txt"));                
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/StressAppTest/StressAppTestMetricsParserTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using VirtualClient.Contracts;
+using VirtualClient.Common.Contracts;
+using VirtualClient.Actions;
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    [TestFixture]
+    [Category("Unit")]
+    class StressAppTestMetricsParserTests
+    {
+        private StressAppTestMetricsParser testParser;
+        string workingDirectory;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Test]
+        public void StressAppTestParserVerifyMetricsForPassResults()
+        {
+            string resultsPath = Path.Combine(this.workingDirectory, "Examples", "StressAppTest", "stressAppTestLog_pass.txt");
+            string rawText = File.ReadAllText(resultsPath);
+            this.testParser = new StressAppTestMetricsParser(rawText);
+            IList<Metric> metrics = this.testParser.Parse();
+
+            Assert.AreEqual(1, metrics.Count);
+            MetricAssert.Exists(metrics, "hardwareErrorCount", 0);
+        }
+
+        [Test]
+        public void StressAppTestParserVerifyMetricsForFailedResults()
+        {
+            string resultsPath = Path.Combine(this.workingDirectory, "Examples", "StressAppTest", "StressAppTestLog_forcedErrors.txt");
+            string rawText = File.ReadAllText(resultsPath);
+            this.testParser = new StressAppTestMetricsParser(rawText);
+            IList<Metric> metrics = this.testParser.Parse();
+
+            Assert.AreEqual(1, metrics.Count);
+            MetricAssert.Exists(metrics, "hardwareErrorCount", 15);
+        }
+    }
+}
