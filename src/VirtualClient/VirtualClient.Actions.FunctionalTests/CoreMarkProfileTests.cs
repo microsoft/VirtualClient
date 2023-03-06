cbc4c6863c8b75ebdaf5455c10853c7696951a44
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2023-03-06 14:16:50 -0800

    Onboard Coremark Pro (#64)
    * Onboard Coremark Pro
    
    * Add tests
    
    * remove csproj change

---
+++ src/VirtualClient/VirtualClient.Actions.FunctionalTests/CoreMarkProfileTests.cs
@@ -0,0 +1,128 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using VirtualClient.Common;
+    using VirtualClient.Contracts;
+
+    [TestFixture]
+    [Category("Functional")]
+    public class CoreMarkProfileTests
+    {
+        private DependencyFixture mockFixture;
+
+        [SetUp]
+        public void SetupFixture()
+        {
+            this.mockFixture = new DependencyFixture();
+            ComponentTypeCache.Instance.LoadComponentTypes(TestDependencies.TestDirectory);
+        }
+
+        [Test]
+        [TestCase("PERF-CPU-COREMARK.json")]
+        public void CoreMarkWorkloadProfileParametersAreInlinedCorrectly(string profile)
+        {
+            this.mockFixture.Setup(PlatformID.Unix);
+            using (ProfileExecutor executor = TestDependencies.CreateProfileExecutor(profile, this.mockFixture.Dependencies))
+            {
+                WorkloadAssert.ParameterReferencesInlined(executor.Profile);
+            }
+        }
+
+        [Test]
+        [TestCase("PERF-CPU-COREMARKPRO.json")]
+        public void CoreMarkProWorkloadProfileParametersAreInlinedCorrectly(string profile)
+        {
+            this.mockFixture.Setup(PlatformID.Unix);
+            using (ProfileExecutor executor = TestDependencies.CreateProfileExecutor(profile, this.mockFixture.Dependencies))
+            {
+                WorkloadAssert.ParameterReferencesInlined(executor.Profile);
+            }
+        }
+
+        [Test]
+        [TestCase("PERF-CPU-COREMARK.json")]
+        public async Task CoreMarkWorkloadProfileExecutesTheExpectedWorkloadsOnUnixPlatform(string profile)
+        {
+            IEnumerable<string> expectedCommands = new List<string>
+            {
+                @"sudo make XCFLAGS=""-DMULTITHREAD=71 -DUSE_PTHREAD"" REBUILD=1 LFLAGS_END=-pthread",
+            };
+
+            this.mockFixture.Setup(PlatformID.Unix);
+            this.mockFixture.SetupDisks(withRemoteDisks: false);
+            this.mockFixture.SetupLinuxPackagesInstalled(new Dictionary<string, string>
+            {
+                { "gcc", "10" }, // Should match profile defaults.
+                { "cc", "10" }
+            });
+
+            this.mockFixture.SystemManagement.Setup(mgr => mgr.GetSystemCoreCount()).Returns(71);
+
+            this.mockFixture.ProcessManager.OnGetProcess = (id) => null;
+            this.mockFixture.ProcessManager.OnCreateProcess = (command, arguments, workingDir) =>
+            {
+                IProcessProxy process = this.mockFixture.CreateProcess(command, arguments, workingDir);
+                if (arguments.Contains("XCFLAGS", StringComparison.OrdinalIgnoreCase))
+                {
+                    process.StandardOutput.Append(TestDependencies.GetResourceFileContents("Results_Coremark.txt"));
+                }
+
+                return process;
+            };
+
+            using (ProfileExecutor executor = TestDependencies.CreateProfileExecutor(profile, this.mockFixture.Dependencies))
+            {
+                await executor.ExecuteAsync(ProfileTiming.OneIteration(), CancellationToken.None).ConfigureAwait(false);
+
+                WorkloadAssert.CommandsExecuted(this.mockFixture, expectedCommands.ToArray());
+            }
+        }
+
+        [Test]
+        [TestCase("PERF-CPU-COREMARKPRO.json")]
+        public async Task CoreMarkProWorkloadProfileExecutesTheExpectedWorkloadsOnUnixPlatform(string profile)
+        {
+            IEnumerable<string> expectedCommands = new List<string>
+            {
+                @"make TARGET=linux64 XCMD='-c71' certify-all"
+            };
+
+            this.mockFixture.Setup(PlatformID.Unix);
+            this.mockFixture.SetupDisks(withRemoteDisks: false);
+            this.mockFixture.SetupLinuxPackagesInstalled(new Dictionary<string, string>
+            {
+                { "gcc", "10" }, // Should match profile defaults.
+                { "cc", "10" }
+            });
+
+            this.mockFixture.SystemManagement.Setup(mgr => mgr.GetSystemCoreCount()).Returns(71);
+
+            this.mockFixture.ProcessManager.OnGetProcess = (id) => null;
+            this.mockFixture.ProcessManager.OnCreateProcess = (command, arguments, workingDir) =>
+            {
+                IProcessProxy process = this.mockFixture.CreateProcess(command, arguments, workingDir);
+                if (arguments.Contains("certify-all", StringComparison.OrdinalIgnoreCase))
+                {
+                    process.StandardOutput.Append(TestDependencies.GetResourceFileContents("Results_CoremarkPro.txt"));
+                }
+
+                return process;
+            };
+
+            using (ProfileExecutor executor = TestDependencies.CreateProfileExecutor(profile, this.mockFixture.Dependencies))
+            {
+                await executor.ExecuteAsync(ProfileTiming.OneIteration(), CancellationToken.None).ConfigureAwait(false);
+
+                WorkloadAssert.CommandsExecuted(this.mockFixture, expectedCommands.ToArray());
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/CoreMark/CoreMarkProExecutorTests.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using VirtualClient.Common;
+    using Moq;
+    using Newtonsoft.Json.Linq;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using VirtualClient.Contracts;
+
+    [TestFixture]
+    [Category("Unit")]
+    public class CoreMarkProExecutorTests
+    {
+        private MockFixture mockFixture;
+        private ConcurrentBuffer coremarkProOutput = new ConcurrentBuffer();
+
+        [SetUp]
+        public void SetupDefaultBehavior()
+        {
+            this.mockFixture = new MockFixture();
+            this.mockFixture.Setup(PlatformID.Unix);
+            this.mockFixture.Parameters["PackageName"] = "CoreMarkPro";
+
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string resultsPath = Path.Combine(currentDirectory, "Examples", "CoreMark", "CoreMarkProExample1.txt");
+            string results = File.ReadAllText(resultsPath);
+            this.coremarkProOutput.Clear();
+            this.coremarkProOutput.Append(results);
+        }
+
+        [Test]
+        public void CoreMarkProExecutorThrowsOnNonSupportedPlatform()
+        {
+            this.mockFixture.Setup(PlatformID.Win32NT);
+            this.mockFixture.Parameters["PackageName"] = "CoreMarkPro";
+
+            using (var executor = new CoreMarkProExecutor(this.mockFixture.Dependencies, this.mockFixture.Parameters))
+            {
+                var workloadException = Assert.ThrowsAsync<WorkloadException>(() => executor.ExecuteAsync(CancellationToken.None));
+                Assert.IsTrue(workloadException.Reason == ErrorReason.PlatformNotSupported);
+            }
+        }
+
+        [Test]
+        public async Task CoreMarkProExecutorExcutesAsExpected()
+        {
+            this.mockFixture.SystemManagement.Setup(mgr => mgr.GetSystemCoreCount()).Returns(71);
+
+            this.mockFixture.ProcessManager.OnCreateProcess = (cmd, args, wd) =>
+            {
+                Assert.AreEqual("make", cmd);
+                Assert.AreEqual(args, $"TARGET=linux64 XCMD='-c71' certify-all");
+                this.mockFixture.Process.StandardOutput = this.coremarkProOutput;
+                return this.mockFixture.Process;
+            };
+
+            using (CoreMarkProExecutor executor = new CoreMarkProExecutor(this.mockFixture.Dependencies, this.mockFixture.Parameters))
+            {
+                await executor.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
+            }
+        }
+    }
+}
