704e2d3988f99fc57a42084ebd945a37946d7332
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2023-03-13 17:56:38 -0700

    Fix lspci that would retry rapidly

--- src/VirtualClient/VirtualClient.Monitors/Lspci/LspciMonitor.cs
+++ src/VirtualClient/VirtualClient.Monitors/Lspci/LspciMonitor.cs
@@ -6,12 +6,14 @@ namespace VirtualClient.Monitors
     using System;
     using System.Collections.Generic;
     using System.Drawing;
+    using System.IO;
     using System.IO.Abstractions;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using global::VirtualClient;
     using global::VirtualClient.Contracts;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
     using VirtualClient.Common;
@@ -77,7 +79,7 @@ namespace VirtualClient.Monitors
             ISystemManagement systemManagement = this.Dependencies.GetService<ISystemManagement>();
             IFileSystem fileSystem = systemManagement.FileSystem;
 
-            string command = "lspci";
+            string command = (this.Platform == PlatformID.Unix) ? "lspci" : "lspci.exe";
             string commandArguments = "-vvv";
 
             await Task.Delay(this.MonitorWarmupPeriod, cancellationToken)
@@ -92,9 +94,10 @@ namespace VirtualClient.Monitors
                     {
                         DependencyPath lspciPackage = await systemManagement.PackageManager.GetPackageAsync(LspciMonitor.Lspci, CancellationToken.None).ConfigureAwait(false);
                         workingDir = this.PlatformSpecifics.ToPlatformSpecificPath(lspciPackage, this.Platform, this.CpuArchitecture).Path;
+                        command = this.PlatformSpecifics.Combine(workingDir, command);
                     }
 
-                    using (IProcessProxy process = systemManagement.ProcessManager.CreateElevatedProcess(this.Platform, command, $"{commandArguments}", workingDir))
+                    using (IProcessProxy process = systemManagement.ProcessManager.CreateElevatedProcess(this.Platform, workingDir + "\\" + command, $"{commandArguments}", workingDir))
                     {
                         this.CleanupTasks.Add(() => process.SafeKill());
 
@@ -148,6 +151,8 @@ namespace VirtualClient.Monitors
                 catch (Exception exc)
                 {
                     this.Logger.LogErrorMessage(exc, telemetryContext, LogLevel.Warning);
+                    // Always wait for the monitor frequency for the next execution.
+                    await Task.Delay(this.MonitorFrequency).ConfigureAwait(false);
                 }
             }
         }
