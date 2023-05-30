7cab90409df25b03a2f1b805d154136c7843c08f
nikhitha malkapuram <nmalkapuram@microsoft.com>
2023-05-30 12:40:59 +0530

    Adding bufferWait dependency

---
+++ src/VirtualClient/VirtualClient.Dependencies.UnitTests/BufferTimeWaiterTests.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Dependencies
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Moq;
+    using NUnit.Framework;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    [TestFixture]
+    [Category("Unit")]
+    internal class BufferTimeWaiterTests
+    {
+        private MockFixture mockFixture;
+
+        public void SetupDefaults(PlatformID platform, Architecture architecture = Architecture.X64)
+        {
+            this.mockFixture = new MockFixture();
+            this.mockFixture.Setup(platform, architecture);
+        }
+
+        [Test]
+        [TestCase(PlatformID.Unix, Architecture.X64)]
+        public async Task BufferTimeWaiterWaitsForExpectedAmountOfTime(PlatformID platform, Architecture architecture)
+        {
+            this.SetupDefaults(platform, architecture);
+            this.mockFixture.Parameters[nameof(BufferTimeWaiter.BufferTimeInSec)] = 1;
+
+            using (TestBufferTimeWaiter testBufferTimeWaiter = new TestBufferTimeWaiter(this.mockFixture))
+            {
+                DateTime startTime = DateTime.Now;
+                await testBufferTimeWaiter.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
+                DateTime endTime = DateTime.Now;
+                if ((endTime - startTime).TotalSeconds > long.Parse(this.mockFixture.Parameters[nameof(BufferTimeWaiter.BufferTimeInSec)].ToString()))
+                {
+                    Assert.IsTrue(true);
+                }
+                else
+                {
+                    Assert.IsTrue(false);
+                }
+            }
+        }
+
+        private class TestBufferTimeWaiter : BufferTimeWaiter
+        {
+            public TestBufferTimeWaiter(MockFixture mockFixture)
+                : base(mockFixture?.Dependencies, mockFixture?.Parameters)
+            {
+            }
+
+            public new Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+            {
+                return base.InitializeAsync(telemetryContext, cancellationToken);
+            }
+
+            public new Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+            {
+                return base.ExecuteAsync(telemetryContext, cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Dependencies/BufferTimeWaiter.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Dependencies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Polly;
+    using VirtualClient.Common;
+    using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// Provides functionality to wait for given time..
+    /// </summary>
+    public class BufferTimeWaiter : VirtualClientComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BufferTimeWaiter"/> class.
+        /// </summary>
+        /// <param name="dependencies">An enumeration of dependencies that can be used for dependency injection.</param>
+        /// <param name="parameters">A series of key value pairs that dictate runtime execution.</param>
+        public BufferTimeWaiter(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
+            : base(dependencies, parameters)
+        { 
+        }
+
+        /// <summary>
+        /// The time in seconds to wait.
+        /// </summary>
+        public int BufferTimeInSec
+        {
+            get
+            {
+                return this.Parameters.GetValue<int>(nameof(this.BufferTimeInSec), 2);
+            }
+        }
+
+        /// <summary>
+        /// Executes Wait operation step.
+        /// </summary>
+        /// <param name="telemetryContext">Provides context information that will be captured with telemetry events.</param>
+        /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
+        /// <returns></returns>
+        protected override async Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            TimeSpan waitTime = TimeSpan.FromSeconds(this.BufferTimeInSec);
+            await Task.Delay(waitTime, cancellationToken).ConfigureAwait();
+            return;
+        }
+    }
+}
