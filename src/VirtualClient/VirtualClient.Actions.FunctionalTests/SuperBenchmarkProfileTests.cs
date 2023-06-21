39daadb149e926137def218e4f881530c1b4658a
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2023-06-21 10:42:53 -0700

    Remove --yes from the gpg signed list (#117)
    * Add --yes option to gpg command in cuda kit install
    
    * Remove --yes from the gpg signed list

--- src/VirtualClient/VirtualClient.Actions.FunctionalTests/SuperBenchmarkProfileTests.cs
+++ src/VirtualClient/VirtualClient.Actions.FunctionalTests/SuperBenchmarkProfileTests.cs
@@ -116,7 +116,7 @@ namespace VirtualClient.Actions
                 "curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | " +
                 "sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg --yes --no-tty && " +
                 "curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | " +
-                "sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg --yes --no-tty] https://#g' |  " +
+                "sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |  " +
                 "sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list";
 
             return new List<string>
--- src/VirtualClient/VirtualClient.Dependencies.UnitTests/NvidiaContainerToolkitInstallationTests.cs
+++ src/VirtualClient/VirtualClient.Dependencies.UnitTests/NvidiaContainerToolkitInstallationTests.cs
@@ -31,7 +31,7 @@ namespace VirtualClient.Dependencies
             "curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | " +
             "sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg --yes --no-tty && " +
             "curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | " +
-            "sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg --yes --no-tty] https://#g' | " +
+            "sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | " +
             " sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list";
 
         private const string UpdateCommand = "apt-get update";
