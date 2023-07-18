2d80b563b4edd3836bc1219ae55b1c5e2537ccf8
Shobhakumari0502 <138695453+Shobhakumari0502@users.noreply.github.com>
2023-07-18 23:19:31 +0530

    Onboarding furmark (#129)
    * onboarding furmark
    
    * added_temperaure
    
    * error resolved+ added temperature support
    
    * running in session 1
    
    * AMD driver dependency removed + multiple Scenarios added
    
    * removed Driver dependency(drivers should be preinstalled
    
    * small changes + specified for AMD
    
    * Resolving comments and adding documentation
    
    * updating documentation
    
    ---------
    
    Co-authored-by: nikhitha malkapuram <nmalkapuram@microsoft.com>

---
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/Furmark/FurmarkExecutorTest.cs
@@ -0,0 +1,175 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using VirtualClient.Contracts;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Threading;
+    using System.Reflection;
+    using System.IO;
+    using Moq;
+    using System.Runtime.InteropServices;
+    using System.Text.RegularExpressions;
+    using VirtualClient.Common.Telemetry;
+    using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+    [TestFixture]
+    [Category("Unit")]
+    public class FurmarkExecutorTests
+    {
+        private MockFixture fixture;
+        private DependencyPath mockPath;
+        private DependencyPath currentDirectoryPath;
+
+        private string resultsPath;
+        private string rawString;
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            this.fixture = new MockFixture();
+            this.mockPath = this.fixture.Create<DependencyPath>();
+        }
+        [Test]
+        [TestCase(PlatformID.Win32NT, Architecture.X64)]
+        public async Task FurmarkExecutorInitializesItsDependenciesAsExpected(PlatformID platform, Architecture architecture)
+        {
+            this.SetupDefaultMockBehavior(platform, architecture);
+            using (TestFurmarkxecutor executor = new TestFurmarkxecutor(this.fixture))
+            {
+                this.fixture.ProcessManager.OnCreateProcess = (command, arguments, workingDirectory) =>
+                {
+                    return this.fixture.Process;
+                };
+
+                await executor.InitializeAsync(EventContext.None, CancellationToken.None)
+                    .ConfigureAwait(false);
+
+                string expectedScriptFilePath = this.fixture.PlatformSpecifics.Combine(
+                    this.mockPath.Path,"win-x64", "Geeks3D", "Benchmarks", "FurMark", "Furmark");
+
+                Assert.AreEqual(expectedScriptFilePath, executor.ExecutableLocation);
+            }
+        }
+
+        [Test]
+        [TestCase(PlatformID.Win32NT, Architecture.X64)]
+        public async Task FurmarkExecutorExecutesWorkloadAsExpected(PlatformID platform, Architecture architecture)
+        {
+            this.SetupDefaultMockBehavior(platform, architecture);
+            this.fixture.Parameters["Time"] = "10";
+            this.fixture.Parameters["Width"] = "100";
+            this.fixture.Parameters["Height"] = "200";
+            this.fixture.Parameters["Antialiasing"] = "4";
+
+            using (TestFurmarkxecutor executor = new TestFurmarkxecutor(this.fixture))
+            {
+                string expectedFilePath = this.fixture.PlatformSpecifics.Combine(this.mockPath.Path);
+                int executed = 0;
+                if (platform == PlatformID.Win32NT)
+                {
+                    string ExpectedPsexecCommand = this.fixture.PlatformSpecifics.Combine(this.mockPath.Path, "win-x64", "psexec.exe");
+                    string furmarkCommand = this.fixture.PlatformSpecifics.Combine(this.mockPath.Path ,"win-x64","Geeks3D" ,"Benchmarks" ,"FurMark" ,"Furmark");
+                    string packageDir = this.fixture.PlatformSpecifics.Combine(expectedFilePath, "win-x64");
+                    // packageDir = Regex.Replace(packageDir, @":", string.Empty);
+
+
+                    // string expectedmakeCommandArguments = @$"C:\Program Files (x86)\Geeks3D\Benchmarks\FurMark\Furmark";
+                    string executeScriptCommandArguments = $"-accepteula -s -i 1 -w {packageDir} {furmarkCommand} /width={this.fixture.Parameters["Width"]} /height={this.fixture.Parameters["Height"]} /Antialiasing={this.fixture.Parameters["Antialiasing"]} /max_time={this.fixture.Parameters["Time"]} /nogui /nomenubar /noscore /run_mode=1 /log_score /disable_catalyst_warning /log_temperature /max_frames";
+
+                    // string executeScriptCommandArguments = $"/width={this.fixture.Parameters["Width"]} /height={this.fixture.Parameters["Height"]} /Antialiasing={this.fixture.Parameters["Mssa"]} /max_time={this.fixture.Parameters["Time"]} /nogui /nomenubar /noscore /run_mode=1 /log_score /disable_catalyst_warning /log_temperature /max_frames";
+
+                    this.fixture.ProcessManager.OnCreateProcess = (command, arguments, workingDirectory) =>
+                    {
+                        if (arguments == executeScriptCommandArguments && command == ExpectedPsexecCommand)
+                        {
+                            executed++;
+                        }
+                        
+                        return this.fixture.Process;
+                    };
+
+                    await executor.ExecuteAsync(EventContext.None, CancellationToken.None)
+                        .ConfigureAwait(false);
+
+                }
+
+                Assert.AreEqual(1, executed);
+            }
+        }
+
+        [Test]
+        public void FurmarkExecutorThrowsWhenTheResultsFileIsNotGenerated()
+        {
+            this.SetupDefaultMockBehavior();
+
+            using (TestFurmarkxecutor executor = new TestFurmarkxecutor(this.fixture))
+            {
+                this.fixture.ProcessManager.OnCreateProcess = (file, arguments, workingDirectory) =>
+                {
+                    this.fixture.FileSystem.Setup(fe => fe.File.Exists(executor.ResultsFilePath)).Returns(false);
+                    this.fixture.Process.StandardError.Append("123");
+                    return this.fixture.Process;
+                };
+                WorkloadResultsException exception = Assert.ThrowsAsync<WorkloadResultsException>(
+                     () => executor.ExecuteAsync(EventContext.None, CancellationToken.None));
+
+                Assert.AreEqual(ErrorReason.WorkloadFailed, exception.Reason);
+            }
+        }
+
+        private void SetupDefaultMockBehavior(PlatformID platform = PlatformID.Win32NT, Architecture architecture = Architecture.X64)
+        {
+            this.fixture.Setup(platform, architecture);
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            this.currentDirectoryPath = new DependencyPath("Furmark", currentDirectory);
+            this.fixture.FileSystem.Setup(fe => fe.File.Exists(It.IsAny<string>())).Returns(true);
+            this.fixture.FileSystem.Setup(fe => fe.File.Exists(null)).Returns(false);
+
+            resultsPath = this.fixture.PlatformSpecifics.Combine(this.currentDirectoryPath.Path, @"Examples\Furmark\FurmarkExampleResults.txt");
+            this.rawString = File.ReadAllText(resultsPath);
+
+            this.fixture.FileSystem.Setup(rt => rt.File.ReadAllTextAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(this.rawString);
+
+            this.fixture.PackageManager.OnGetPackage().ReturnsAsync(this.mockPath);
+            this.fixture.ProcessManager.OnCreateProcess = (command, arguments, directory) => this.fixture.Process;
+
+            this.fixture.Parameters["PackageName"] = "Furmark";
+            this.fixture.Parameters["PsExecPackageName"] = "pstools";
+        }
+
+        private class TestFurmarkxecutor : FurmarkExecutor
+        {
+            public TestFurmarkxecutor(MockFixture fixture)
+                : base(fixture.Dependencies, fixture.Parameters)
+            {
+            }
+
+            public TestFurmarkxecutor(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
+                : base(dependencies, parameters)
+            {
+            }
+
+
+            public new Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+            {
+                return base.InitializeAsync(telemetryContext, cancellationToken);
+            }
+
+            public new Task ExecuteAsync(EventContext context, CancellationToken cancellationToken)
+            {
+                this.InitializeAsync(context, cancellationToken).GetAwaiter().GetResult();
+                return base.ExecuteAsync(context, cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Dependencies/AMDGPUDriverInstallation.cs
@@ -0,0 +1,119 @@
+﻿namespace VirtualClient.Dependencies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.IO.Abstractions;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.Extensions.DependencyInjection;
+    using Polly;
+    using VirtualClient.Common;
+    using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Rest;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// Installation component for AMD GPU Drivers
+    /// </summary>
+    public class AMDGPUDriverInstallation : VirtualClientComponent
+    {
+        private IPackageManager packageManager;
+        private IFileSystem fileSystem;
+        private ISystemManagement systemManager;
+        private IStateManager stateManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AMDGPUDriverInstallation"/> class.
+        /// </summary>
+        /// <param name="dependencies">An enumeration of dependencies that can be used for dependency injection.</param>
+        /// <param name="parameters">A series of key value pairs that dictate runtime execution.</param>
+        public AMDGPUDriverInstallation(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
+            : base(dependencies, parameters)
+        {
+            dependencies.ThrowIfNull(nameof(dependencies));
+            this.RetryPolicy = Policy.Handle<Exception>().WaitAndRetryAsync(5, (retries) => TimeSpan.FromSeconds(retries + 1));
+            this.systemManager = dependencies.GetService<ISystemManagement>();
+            this.stateManager = this.systemManager.StateManager;
+            this.fileSystem = this.systemManager.FileSystem;
+            this.packageManager = this.systemManager.PackageManager;
+        }
+
+        /// <summary>
+        /// Determines whether Reboot is required or not after Driver installation
+        /// </summary>
+        public bool RebootRequired
+        {
+            get
+            {
+                switch (this.Platform)
+                {
+                    case PlatformID.Win32NT:
+                        return this.Parameters.GetValue<bool>(nameof(AMDGPUDriverInstallation.RebootRequired), false);
+                    default:
+                        return this.Parameters.GetValue<bool>(nameof(AMDGPUDriverInstallation.RebootRequired), true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A policy that defines how the component will retry when
+        /// it experiences transient issues.
+        /// </summary>
+        public IAsyncPolicy RetryPolicy { get; set; }
+
+        /// <summary>
+        /// Executes  GPU driver installation steps.
+        /// </summary>
+        /// <returns></returns>
+        protected override Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            return this.Logger.LogMessageAsync($"{this.TypeName}.ExecuteAMDDriverInstallation", telemetryContext, async () =>
+            {
+                State installationState = await this.stateManager.GetStateAsync<State>(nameof(AMDGPUDriverInstallation), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (installationState == null)
+                {
+                    if (this.Platform == PlatformID.Win32NT)
+                    {
+                        await this.AMDDriverInstallation(telemetryContext, cancellationToken)
+                                   .ConfigureAwait(false);
+
+                        await this.stateManager.SaveStateAsync(nameof(this.AMDDriverInstallation), new State(), cancellationToken)
+                            .ConfigureAwait(false);
+
+                    }
+
+                    VirtualClientRuntime.IsRebootRequested = this.RebootRequired;
+                }
+            });
+        }
+
+        private async Task AMDDriverInstallation(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            string installerPath = string.Empty;
+
+            DependencyPath amdDriverInstallerPackage = await this.packageManager.GetPackageAsync(
+                this.PackageName, cancellationToken)
+                    .ConfigureAwait(false);
+
+            if (this.fileSystem.Directory.GetFiles(amdDriverInstallerPackage.Path, "*.exe", SearchOption.AllDirectories).Length > 0)
+            {
+                installerPath = this.fileSystem.Directory.GetFiles(amdDriverInstallerPackage.Path, "*.exe", SearchOption.AllDirectories)[0];
+            }
+            else
+            {
+                throw new DependencyException($"The installer file was not found in the directory {amdDriverInstallerPackage.Path}", ErrorReason.DependencyNotFound);
+            }
+
+            await this.ExecuteCommandAsync(installerPath, "/S /v/qn", Environment.CurrentDirectory, telemetryContext, cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
