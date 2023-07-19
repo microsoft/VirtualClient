4d2d6fc3c21ec026ffb6ebccf61144427d461e73
Prachi Singla <prachisingla@microsoft.com>
2023-07-19 16:15:45 +0530

    Adding dependency to install AMD GPU Driver

--- src/VirtualClient/VirtualClient.Dependencies/AMDGPUDriverInstallation.cs
+++ src/VirtualClient/VirtualClient.Dependencies/AMDGPUDriverInstallation.cs
@@ -23,6 +23,8 @@
     /// </summary>
     public class AMDGPUDriverInstallation : VirtualClientComponent
     {
+        private const string Nvv4ExeName = "AMD-Azure-NVv4-Driver-22Q2.exe";
+        private const string V620ExeName = "Setup.exe";
         private IPackageManager packageManager;
         private IFileSystem fileSystem;
         private ISystemManagement systemManager;
@@ -62,6 +64,17 @@
         }
 
         /// <summary>
+        /// Vm series on which driver is installed. (e.g. nvv4, v620)
+        /// </summary>
+        public string VmSeries
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(AMDGPUDriverInstallation.VmSeries), "nvv4");
+            }
+        }
+
+        /// <summary>
         /// A policy that defines how the component will retry when
         /// it experiences transient issues.
         /// </summary>
@@ -73,7 +86,7 @@
         /// <returns></returns>
         protected override Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
         {
-            return this.Logger.LogMessageAsync($"{this.TypeName}.ExecuteAMDDriverInstallation", telemetryContext, async () =>
+            return this.Logger.LogMessageAsync($"{this.TypeName}.Execute", telemetryContext, async () =>
             {
                 State installationState = await this.stateManager.GetStateAsync<State>(nameof(AMDGPUDriverInstallation), cancellationToken)
                     .ConfigureAwait(false);
@@ -82,12 +95,18 @@
                 {
                     if (this.Platform == PlatformID.Win32NT)
                     {
-                        await this.AMDDriverInstallation(telemetryContext, cancellationToken)
+                        await this.InstallAMDGPUDriver(telemetryContext, cancellationToken)
                                    .ConfigureAwait(false);
 
-                        await this.stateManager.SaveStateAsync(nameof(this.AMDDriverInstallation), new State(), cancellationToken)
+                        await this.stateManager.SaveStateAsync(nameof(AMDGPUDriverInstallation), new State(), cancellationToken)
                             .ConfigureAwait(false);
 
+                    }
+                    else if (this.Platform == PlatformID.Unix)
+                    {
+                        throw new DependencyException(
+                                $"AMD GPU Driver Installation is not supported by Virtual Client on the current platform '{this.Platform}'",
+                                ErrorReason.PlatformNotSupported);
                     }
 
                     VirtualClientRuntime.IsRebootRequested = this.RebootRequired;
@@ -95,7 +114,7 @@
             });
         }
 
-        private async Task AMDDriverInstallation(EventContext telemetryContext, CancellationToken cancellationToken)
+        private async Task InstallAMDGPUDriver(EventContext telemetryContext, CancellationToken cancellationToken)
         {
             string installerPath = string.Empty;
 
@@ -103,17 +122,72 @@
                 this.PackageName, cancellationToken)
                     .ConfigureAwait(false);
 
-            if (this.fileSystem.Directory.GetFiles(amdDriverInstallerPackage.Path, "*.exe", SearchOption.AllDirectories).Length > 0)
+            if (amdDriverInstallerPackage == null)
             {
-                installerPath = this.fileSystem.Directory.GetFiles(amdDriverInstallerPackage.Path, "*.exe", SearchOption.AllDirectories)[0];
-            }
-            else
-            {
-                throw new DependencyException($"The installer file was not found in the directory {amdDriverInstallerPackage.Path}", ErrorReason.DependencyNotFound);
+                throw new DependencyException(
+                    $"The expected package '{this.PackageName}' does not exist on the system or is not registered.",
+                    ErrorReason.WorkloadDependencyMissing);
             }
 
-            await this.ExecuteCommandAsync(installerPath, "/S /v/qn", Environment.CurrentDirectory, telemetryContext, cancellationToken)
+            switch (this.VmSeries)
+            {
+                case "nvv4":
+                    {
+                        await this.InstallAMDGPUDriverOnNvv4(amdDriverInstallerPackage.Path, telemetryContext, cancellationToken)
+                            .ConfigureAwait(false);
+                        break;
+                    }
+
+                case "v620":
+                    {
+                        await this.InstallAMDGPUDriverOnV620(amdDriverInstallerPackage.Path, telemetryContext, cancellationToken)
+                            .ConfigureAwait(false);
+                        break;
+                    }
+
+                default:
+                    {
+                        throw new NotSupportedException($"VM Series '{this.VmSeries}' is not supported.");
+                    }
+            }            
+        }
+
+        private async Task InstallAMDGPUDriverOnNvv4(string driverPackagePath, EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            string installationFile = this.Combine(driverPackagePath, this.VmSeries, AMDGPUDriverInstallation.Nvv4ExeName);
+
+            if (!this.fileSystem.File.Exists(installationFile))
+            { 
+                throw new DependencyException($"The installer file was not found in the directory {driverPackagePath}", ErrorReason.DependencyNotFound);
+            }
+
+            IProcessProxy process = await this.ExecuteCommandAsync(installationFile, "/S /v/qn", Environment.CurrentDirectory, telemetryContext, cancellationToken)
                 .ConfigureAwait(false);
+
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                await this.LogProcessDetailsAsync(process, telemetryContext).ConfigureAwait(false);
+                process.ThrowIfErrored<DependencyException>(ProcessProxy.DefaultSuccessCodes, errorReason: ErrorReason.DependencyInstallationFailed);
+            }
+        }
+
+        private async Task InstallAMDGPUDriverOnV620(string driverPackagePath, EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            string installationFile = this.Combine(driverPackagePath, this.VmSeries, AMDGPUDriverInstallation.V620ExeName);
+
+            if (!this.fileSystem.File.Exists(installationFile))
+            {
+                throw new DependencyException($"The installer file was not found in the directory {driverPackagePath}", ErrorReason.DependencyNotFound);
+            }
+
+            IProcessProxy process = await this.ExecuteCommandAsync(installationFile, "-INSTALL -OUTPUT screen", Environment.CurrentDirectory, telemetryContext, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                await this.LogProcessDetailsAsync(process, telemetryContext).ConfigureAwait(false);
+                process.ThrowIfErrored<DependencyException>(ProcessProxy.DefaultSuccessCodes, errorReason: ErrorReason.DependencyInstallationFailed);
+            }
         }
     }
 }
