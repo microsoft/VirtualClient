961372c2e9cf45ee1596c0f0c45e5f03329a79a2
Prachi <prachisinghal1210@gmail.com>
2023-08-29 00:19:23 +0530

    Users/prachisingla/onboarding CTS traffic WL  (#142)
    * Onboarding CtsTraffic WL
    
    * Add docs
    
    * Adding few changes
    
    * Adding cts traffic workload
    
    * Adding few documentations
    
    * fixing few comments
    
    * Updating unit tests
    
    * Correcting tests
    
    * Updating notes
    
    * Incorporating review comments
    
    * recmoving unnecessary file
    
    * Adding csproj change
    
    * Incorporating few comments
    
    * Removing unnecessary char
    
    ---------
    
    Co-authored-by: Prachi Singla <prachisingla@microsoft.com>

---
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/CtsTraffic/CtsTrafficServerExecutorTests.cs
@@ -0,0 +1,195 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Moq;
+    using NUnit.Framework;
+    using Polly;
+    using VirtualClient.Common.Contracts;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+    using static VirtualClient.Actions.CtsTrafficExecutor;
+
+    [TestFixture]
+    [Category("Unit")]
+    public class CtsTrafficServerExecutorTests
+    {
+        private MockFixture mockFixture;
+        private DependencyPath mockCtsTrafficPackage;
+        private string mockResults;
+
+        public void SetupDefaults(PlatformID platform = PlatformID.Win32NT, Architecture architecture = Architecture.X64, int numaNodeIndex = 0)
+        {
+            this.mockFixture = new MockFixture();
+            this.mockFixture.Setup(platform, architecture);
+
+            this.mockCtsTrafficPackage = new DependencyPath("ctstraffic", this.mockFixture.GetPackagePath("ctstraffic"));
+
+            this.mockFixture.Parameters = new Dictionary<string, IConvertible>()
+            {
+                ["PackageName"] = this.mockCtsTrafficPackage.Name,
+                ["PrimaryPort"] = "4445",
+                ["SecondaryPort"] = "4444",
+                ["NumaNodeIndex"] = numaNodeIndex,
+                ["BufferInBytes"] = 36654,
+                ["Pattern"] = "Duplex",
+                ["BytesToTransfer"]= "0x400000000",
+                ["ServerExitLimit"] = 1
+            };
+
+            this.mockFixture.PackageManager.OnGetPackage("ctstraffic").ReturnsAsync(this.mockCtsTrafficPackage);
+
+            // Setup:
+            // The server will be checking for state objects. State is how the server communicates required information
+            // to the client.
+            CtsTrafficServerState state = new CtsTrafficServerState();
+            this.mockFixture.ApiClient.OnGetState(nameof(CtsTrafficServerState))
+                .ReturnsAsync(() => this.mockFixture.CreateHttpResponse(HttpStatusCode.OK, new Item<CtsTrafficServerState>(nameof(CtsTrafficServerState), state)));
+
+            this.mockFixture.ApiClient.OnUpdateState<CtsTrafficServerState>(nameof(CtsTrafficServerState))
+                .ReturnsAsync(() => this.mockFixture.CreateHttpResponse(HttpStatusCode.OK));
+
+            this.mockFixture.File.Setup(f => f.Exists(It.IsAny<string>())).Returns(true);
+            this.mockFixture.Directory.Setup(d => d.Exists(It.IsAny<string>())).Returns(true);
+
+            this.mockResults = File.ReadAllText(Path.Combine(MockFixture.ExamplesDirectory, @"CtsTraffic", "CtsTrafficResultsExample.csv"));
+
+            this.mockFixture.File.Setup(f => f.ReadAllTextAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(this.mockResults);
+
+        }
+
+        [Test]
+        public async Task CtsTrafficServerExecutorWritesTheExpectedInformationToTheServerState()
+        {
+            this.SetupDefaults();
+
+            using (var executor = new TestCtsTrafficServerExecutor(this.mockFixture.Dependencies, this.mockFixture.Parameters))
+            {
+                bool confirmed_setup = true;
+                this.mockFixture.ApiClient.OnUpdateState<CtsTrafficServerState>(nameof(CtsTrafficServerState))
+                    .Callback<string, object, CancellationToken, IAsyncPolicy<HttpResponseMessage>>((stateId, state, token, retryPolicy) =>
+                    {
+                        Item<CtsTrafficServerState> actualState = state as Item<CtsTrafficServerState>;
+
+                        // Based on setup at the top. On first call, the database has not been created yet.
+                        Assert.IsNotNull(actualState);
+                        if (actualState.Definition.ServerSetupCompleted)
+                        {
+                            confirmed_setup = true;
+                        }
+                    })
+                    .ReturnsAsync(this.mockFixture.CreateHttpResponse(HttpStatusCode.OK));
+
+                await executor.ExecuteAsync(CancellationToken.None);
+                Assert.IsTrue(confirmed_setup);
+            }
+        }
+
+        [Test]
+        [TestCase(Architecture.X64)]
+        [TestCase(Architecture.Arm64)]
+        public async Task CtsTrafficServerExecutorExecutesExpectedCommandsOnWindowsSystemsWithNumaNodeProvided(Architecture architecture)
+        {
+            this.SetupDefaults(PlatformID.Win32NT,architecture);
+            using (var executor = new TestCtsTrafficServerExecutor(this.mockFixture.Dependencies, this.mockFixture.Parameters))
+            {
+                // e.g.
+                // C:\Users\Any\VirtualClient\packages\ctstraffic
+                string ctsTrafficPackage = this.mockCtsTrafficPackage.Path;
+                string arch = architecture.ToString().ToLower();
+
+                List<string> expectedCommands = new List<string>()
+                {
+                    // Format:
+                    // {command} {command_arguments} --> {working_dir}
+                    $"{ctsTrafficPackage}\\win-{arch}\\StartProcessInNumaNode.exe {executor.NumaNodeIndex} " +
+                    $"\"{ctsTrafficPackage}\\win-{arch}\\ctsTraffic.exe -Listen:* -Consoleverbosity:1 " +
+                    $"-StatusFilename:{ctsTrafficPackage}\\win-{arch}\\Results\\Status.csv -ConnectionFilename:{ctsTrafficPackage}\\win-{arch}\\Results\\Connections.csv " +
+                    $"-ErrorFileName:{ctsTrafficPackage}\\win-{arch}\\Results\\Errors.txt -Port:{executor.Port} -Pattern:{executor.Pattern} " +
+                    $"-Transfer:{executor.BytesToTransfer} -ServerExitLimit:{executor.ServerExitLimit} -Buffer:{executor.BufferInBytes} -TimeLimit:150000\" --> {ctsTrafficPackage}\\win-{arch}"
+                };
+
+                this.mockFixture.ProcessManager.OnProcessCreated = (process) =>
+                {
+                    process.StandardOutput.Clear();
+                    if (process.FullCommand().Contains("ctsTraffic.exe"))
+                    {
+                        process.StandardOutput.Append(this.mockResults);
+                    }
+                    expectedCommands.Remove($"{process.FullCommand()} --> {process.StartInfo.WorkingDirectory}");
+                };
+
+                await executor.ExecuteAsync(CancellationToken.None);
+                Assert.IsEmpty(expectedCommands);
+            }
+        }
+
+        [Test]
+        [TestCase(Architecture.X64)]
+        [TestCase(Architecture.Arm64)]
+        public async Task CtsTrafficServerExecutorExecutesExpectedCommandsOnWindowsSystemsWithNumaNodeNotProvided(Architecture architecture)
+        {
+            this.SetupDefaults(PlatformID.Win32NT, architecture, -1);
+            using (var executor = new TestCtsTrafficServerExecutor(this.mockFixture.Dependencies, this.mockFixture.Parameters))
+            {
+                // e.g.
+                // C:\Users\Any\VirtualClient\packages\ctstraffic
+                string ctsTrafficPackage = this.mockCtsTrafficPackage.Path;
+                string arch = architecture.ToString().ToLower();
+
+                List<string> expectedCommands = new List<string>()
+                {
+                    // Format:
+                    // {command} {command_arguments} --> {working_dir}
+                    $"{ctsTrafficPackage}\\win-{arch}\\ctsTraffic.exe -Listen:* -Consoleverbosity:1 " +
+                    $"-StatusFilename:{ctsTrafficPackage}\\win-{arch}\\Results\\Status.csv -ConnectionFilename:{ctsTrafficPackage}\\win-{arch}\\Results\\Connections.csv " +
+                    $"-ErrorFileName:{ctsTrafficPackage}\\win-{arch}\\Results\\Errors.txt -Port:{executor.Port} -Pattern:{executor.Pattern} " +
+                    $"-Transfer:{executor.BytesToTransfer} -ServerExitLimit:{executor.ServerExitLimit} -Buffer:{executor.BufferInBytes} -TimeLimit:150000 --> {ctsTrafficPackage}\\win-{arch}"
+                };
+
+                this.mockFixture.ProcessManager.OnProcessCreated = (process) =>
+                {
+                    process.StandardOutput.Clear();
+                    if (process.FullCommand().Contains("ctsTraffic.exe"))
+                    {
+                        process.StandardOutput.Append(this.mockResults);
+                    }
+                    expectedCommands.Remove($"{process.FullCommand()} --> {process.StartInfo.WorkingDirectory}");
+                };
+
+                await executor.ExecuteAsync(CancellationToken.None);
+                Assert.IsEmpty(expectedCommands);
+            }
+        }
+
+        private class TestCtsTrafficServerExecutor : CtsTrafficServerExecutor
+        {
+            public TestCtsTrafficServerExecutor(IServiceCollection services, IDictionary<string, IConvertible> parameters = null)
+                : base(services, parameters)
+            {
+                
+            }
+
+            public new string CtsTrafficPackagePath => base.CtsTrafficPackagePath;
+
+            public new string CtsTrafficExe => base.CtsTrafficExe;
+
+            public new Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+            {
+                return base.InitializeAsync(telemetryContext, cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Actions/CtsTraffic/CtsTrafficServerExecutor.cs
@@ -0,0 +1,184 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using VirtualClient;
+    using VirtualClient.Common;
+    using VirtualClient.Common.Contracts;
+    using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Platform;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// CtsTraffic Server executor.
+    /// </summary>
+    [WindowsCompatible]
+    public class CtsTrafficServerExecutor : CtsTrafficExecutor
+    {
+        private Item<CtsTrafficServerState> serverState;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CtsTrafficServerExecutor"/> class.
+        /// </summary>
+        /// <param name="dependencies">Provides all of the required dependencies to the Virtual Client component.</param>
+        /// <param name="parameters">An enumeration of key-value pairs that can control the execution of the component.</param>/param>
+        public CtsTrafficServerExecutor(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
+           : base(dependencies, parameters)
+        {
+        }
+
+        /// <summary>
+        /// Parameter defines exactly the number of connections an instance of ctsTraffic should handle before exiting.
+        /// </summary>
+        public int ServerExitLimit
+        {
+            get
+            {
+                return this.Parameters.GetValue<int>(nameof(this.ServerExitLimit), 1);
+            }
+        }
+
+        /// <summary>
+        /// Initializes the workload executor paths and dependencies.
+        /// </summary>
+        /// <param name="telemetryContext">Provides context information that will be captured with telemetry events.</param>
+        /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
+        protected override async Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            this.SetServerOnline(false);
+
+            await base.InitializeAsync(telemetryContext, cancellationToken);
+
+            DependencyPath ctsTrafficPackage = await this.GetPlatformSpecificPackageAsync(this.PackageName, cancellationToken);
+            this.CtsTrafficPackagePath = ctsTrafficPackage.Path;
+        }
+
+        /// <summary>
+        /// Start ctstraffic workload at server side.
+        /// </summary>
+        /// <param name="telemetryContext">Provides context information that will be captured with telemetry events.</param>
+        /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
+        protected override async Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            try
+            {
+                string ctsTrafficCommandArgs = $"-Listen:* -Consoleverbosity:1 -StatusFilename:{this.StatusFileName} " +
+                $@"-ConnectionFilename:{this.ConnectionsFileName} -ErrorFileName:{this.ErrorFileName} -Port:{this.Port} " +
+                $@"-Pattern:{this.Pattern} -Transfer:{this.BytesToTransfer} -ServerExitLimit:{this.ServerExitLimit} " +
+                $@"-Buffer:{this.BufferInBytes} -TimeLimit:150000";
+
+                string numaNodeCommandArgs = $@"{this.NumaNodeIndex} ""{this.CtsTrafficExe} {ctsTrafficCommandArgs}""";
+
+                if (this.NumaNodeIndex == -1)
+                {
+                    await this.ExecuteAndWaitCommandAsync(this.CtsTrafficExe, ctsTrafficCommandArgs, this.CtsTrafficPackagePath, telemetryContext, cancellationToken);
+
+                    using (HttpResponseMessage response = await this.LocalApiClient.DeleteStateAsync(nameof(CtsTrafficServerState), cancellationToken))
+                    {
+                        response.ThrowOnError<WorkloadException>();
+                    }
+                }
+                else
+                {
+                    await this.ExecuteAndWaitCommandAsync(this.ProcessInNumaNodeExe, numaNodeCommandArgs, this.CtsTrafficPackagePath, telemetryContext, cancellationToken);
+
+                    using (HttpResponseMessage response = await this.LocalApiClient.DeleteStateAsync(nameof(CtsTrafficServerState), cancellationToken))
+                    {
+                        response.ThrowOnError<WorkloadException>();
+                    }
+                }
+            }
+            catch
+            {
+                this.SetServerOnline(false);
+
+                using (HttpResponseMessage response = await this.LocalApiClient.DeleteStateAsync(nameof(CtsTrafficServerState), cancellationToken))
+                {
+                    response.ThrowOnError<WorkloadException>();
+                }
+
+                throw;
+            }
+        }
+
+        private async Task ExecuteAndWaitCommandAsync(
+            string command,
+            string commandArguments,
+            string workingDirectory,
+            EventContext telemetryContext,
+            CancellationToken cancellationToken)
+        {
+            command.ThrowIfNullOrWhiteSpace(nameof(command));
+            commandArguments.ThrowIfNullOrWhiteSpace(nameof(commandArguments));
+            workingDirectory.ThrowIfNullOrWhiteSpace(nameof(workingDirectory));
+            telemetryContext.ThrowIfNull(nameof(telemetryContext));
+
+            EventContext relatedContext = telemetryContext.Clone()
+                .AddContext(nameof(command), command)
+                .AddContext(nameof(commandArguments), commandArguments)
+                .AddContext(nameof(workingDirectory), workingDirectory);
+
+            await this.Logger.LogMessageAsync($"{this.TypeName}.ExecuteWorkload", relatedContext, async () =>
+            {
+                IProcessProxy process = null;
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    ProcessManager processManager = this.Dependencies.GetService<ProcessManager>();
+
+                    process = processManager.CreateProcess(command, commandArguments, workingDirectory);
+
+                    this.CleanupTasks.Add(() => process.SafeKill());
+                    this.Logger.LogTraceMessage($"Executing: {command} {commandArguments}".Trim(), relatedContext);
+
+                    if (!process.Start())
+                    {
+                        throw new WorkloadException($"The workload did not start as expected.", ErrorReason.WorkloadFailed);
+                    }
+
+                    this.SetServerOnline(true);
+                    this.SaveServerStateAsync(cancellationToken)
+                        .ConfigureAwait(false);
+
+                    await process.WaitForExitAsync(cancellationToken)
+                        .ConfigureAwait(false);
+
+                    await this.LogProcessDetailsAsync(process, relatedContext, "CtsTraffic", logToFile: true);
+                    process.ThrowIfWorkloadFailed();
+
+                    await this.CaptureMetricsAsync(process, commandArguments, relatedContext, cancellationToken);
+
+                    await this.LocalApiClient.DeleteStateAsync(nameof(CtsTrafficServerState), cancellationToken)
+                        .ConfigureAwait(false);
+                }
+            });
+
+            await this.WaitAsync(TimeSpan.FromSeconds(2), cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task SaveServerStateAsync(CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                this.serverState = await this.LocalApiClient.GetOrCreateStateAsync<CtsTrafficServerState>(
+                    nameof(CtsTrafficServerState),
+                    cancellationToken,
+                    logger: this.Logger);
+
+                this.serverState.Definition.ServerSetupCompleted = true;
+
+                using (HttpResponseMessage response = await this.LocalApiClient.UpdateStateAsync<CtsTrafficServerState>(nameof(CtsTrafficServerState), this.serverState, cancellationToken))
+                {
+                    response.ThrowOnError<WorkloadException>();
+                }
+            }
+        }
+    }
+}
