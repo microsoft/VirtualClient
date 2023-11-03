79738d5f01c205cefdaa167bc8778fc17205833e
Richard Murillo <rimuri@microsoft.com>
2023-11-03 13:24:57 -0700

    Update to dynamically pull Wrathmark, compile, publish, and execute

--- src/VirtualClient/VirtualClient.Actions/Wrathmark/WrathmarkWorkloadExecutor.cs
+++ src/VirtualClient/VirtualClient.Actions/Wrathmark/WrathmarkWorkloadExecutor.cs
@@ -1,16 +1,17 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using VirtualClient.Common.Telemetry;
+﻿using System.IO;
 
 namespace VirtualClient.Actions.Wrathmark
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     using Microsoft.Extensions.DependencyInjection;
-    using Polly.Caching;
 
     using VirtualClient.Common;
     using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Telemetry;
     using VirtualClient.Contracts;
 
     /// <summary>
@@ -20,6 +21,10 @@ namespace VirtualClient.Actions.Wrathmark
     {
         private readonly ISystemManagement systemManagement;
         private readonly ProcessManager processManager;
+        private readonly IPackageManager packageManager;
+
+        private string benchmarkDirectory;
+        private string dotnetExePath;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WrathmarkWorkloadExecutor"/> class.
@@ -34,7 +39,20 @@ namespace VirtualClient.Actions.Wrathmark
             // Follows the example, but is wrong. The services should be injected to this class, not resolved here
             this.systemManagement = dependencies.GetService<ISystemManagement>();
             this.processManager = this.systemManagement.ProcessManager;
+            this.packageManager = this.systemManagement.PackageManager;
         }
+
+        /// <summary>
+        /// The name of the package where the DotNetSDK package is downloaded.
+        /// </summary>
+        public string DotNetSdkPackageName => this.Parameters.GetValue<string>(nameof(WrathmarkWorkloadExecutor.DotNetSdkPackageName), "dotnetsdk");
+
+        /// <summary>
+        /// The name of the package where the AspNetBench package is downloaded.
+        /// </summary>
+        public string TargetFramework =>
+            // Lower case to prevent build path issue.
+            this.Parameters.GetValue<string>(nameof(WrathmarkWorkloadExecutor.TargetFramework)).ToLower();
 
         /// <summary>
         /// Name of the tool.
@@ -51,6 +69,51 @@ namespace VirtualClient.Actions.Wrathmark
         /// </summary>
         protected string WorkloadExecutablePath { get; }
 
+        /// <inheritdoc />
+        protected override async Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            DependencyPath workloadPackage = await this.packageManager.GetPackageAsync(
+                    this.PackageName,
+                    cancellationToken);
+
+            if (workloadPackage == null)
+            {
+                throw new DependencyException(
+                    $"The expected package '{this.PackageName}' does not exist on the system or is not registered.",
+                    ErrorReason.WorkloadDependencyMissing);
+            }
+
+            const string RelativeBenchmarkPath = "wrath-sharp";
+            this.benchmarkDirectory = this.Combine(workloadPackage.Path, RelativeBenchmarkPath);
+
+            DependencyPath dotnetSdkPackage = await this.packageManager.GetPackageAsync(
+                    this.DotNetSdkPackageName,
+                    cancellationToken)
+                .ConfigureAwait(false);
+
+            if (dotnetSdkPackage == null)
+            {
+                throw new DependencyException(
+                    $"The expected package '{this.DotNetSdkPackageName}' does not exist on the system or is not registered.",
+                    ErrorReason.WorkloadDependencyMissing);
+            }
+
+            this.dotnetExePath = this.Combine(dotnetSdkPackage.Path, this.Platform == PlatformID.Unix ? "dotnet" : "dotnet.exe");
+
+            // Build the wrath sharp project
+            // To make native libraries that can be used, enumerate the SupportedPlatforms metadata and call publish for each
+            // Outputs
+            //    bin/Release/net6.0/linux-x64/wrath-sharp.dll
+            //    bin/Release/net6.0/linux-x64/publish/wrath-sharp.dll
+            string publishArgument = $"publish -c Release -r {this.PlatformArchitectureName} -f {this.TargetFramework} /p:UseSharedCompilation=false /p:BuildInParallel=false /m:1 /p:Deterministic=true /p:Optimize=true";
+            await this.ExecuteCommandAsync(
+                    this.dotnetExePath,
+                    publishArgument,
+                    this.benchmarkDirectory,
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+
         /// <summary>
         /// Executes the Wrathmark component logic.
         /// </summary>
@@ -58,25 +121,73 @@ namespace VirtualClient.Actions.Wrathmark
         /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
         protected override async Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
         {
+            // Example: ./bin/Release/net6.0/linux-x64/publish
+            string outputDirectory = Path.Combine(
+                this.benchmarkDirectory,
+                "bin",
+                "Release",
+                this.TargetFramework,
+                this.PlatformArchitectureName,
+                "Publish");
+
             this.Logger.LogTraceMessage(
                 $"{nameof(WrathmarkWorkloadExecutor)}.Starting",
                 telemetryContext);
 
             var startTime = DateTime.UtcNow;
 
-            var results = await this.ExecuteWorkloadAsync(telemetryContext, cancellationToken);
+            var results = await this.ExecuteWorkloadAsync(
+                this.dotnetExePath,
+                $"run {outputDirectory} --framework ${this.TargetFramework}",
+                this.benchmarkDirectory,
+                telemetryContext,
+                cancellationToken);
 
             var endTime = DateTime.UtcNow;
 
             await this.CaptureMetricsAsync(results, startTime, endTime, telemetryContext, cancellationToken);
         }
 
-        private Task<string> ExecuteWorkloadAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        private async Task ExecuteCommandAsync(
+            string pathToExe,
+            string commandLineArguments,
+            string workingDirectory,
+            CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                this.Logger.LogTraceMessage(
+                    $"Executing process '{pathToExe}' '{commandLineArguments}' at directory '{workingDirectory}'.");
+
+                EventContext telemetryContext = EventContext.Persisted()
+                    .AddContext("command", pathToExe)
+                    .AddContext("commandArguments", commandLineArguments);
+
+                using (IProcessProxy process =
+                       this.systemManagement.ProcessManager.CreateElevatedProcess(
+                           this.Platform,
+                           pathToExe,
+                           commandLineArguments,
+                           workingDirectory))
+                {
+                    this.CleanupTasks.Add(() => process.SafeKill());
+                    await process.StartAndWaitAsync(cancellationToken).ConfigureAwait(false);
+
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        await this.LogProcessDetailsAsync(process, telemetryContext);
+                    }
+                }
+            }
+        }
+
+        private Task<string> ExecuteWorkloadAsync(string command, string arguments, string workingDirectory, EventContext telemetryContext, CancellationToken cancellationToken)
         {
             EventContext relatedContext = telemetryContext.Clone()
                 .AddContext("packageName", this.PackageName)
-                .AddContext("packagePath", this.WorkloadPackage.Path)
-                .AddContext("command", this.WorkloadExecutablePath)
+                .AddContext("packagePath", workingDirectory)
+                .AddContext("command", command)
+                .AddContext("arguments", arguments)
                 ;
 
             return this.Logger.LogMessageAsync($"{nameof(WrathmarkWorkloadExecutor)}.ExecuteWorkload", relatedContext, async () =>
@@ -88,7 +199,7 @@ namespace VirtualClient.Actions.Wrathmark
                 {
                     // We create a operating system process to host the executing workload, start it and
                     // wait for it to exit.
-                    using (IProcessProxy workloadProcess = this.processManager.CreateProcess(this.WorkloadExecutablePath, null, this.WorkloadPackage.Path))
+                    using (IProcessProxy workloadProcess = this.processManager.CreateProcess(command, arguments, workingDirectory))
                     {
                         this.CleanupTasks.Add(() => workloadProcess.SafeKill());
 
