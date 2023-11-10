d9d04ec8ad3555e72b002b0eb96946d887306703
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2023-11-09 21:52:19 -0800

    Build Release instead of Debug in build.cmd (#210)
    * Build Release instead of Debug in build.cmd
    
    * fix test

--- src/VirtualClient/build-packages-workloads.cmd
+++ src/VirtualClient/build-packages-workloads.cmd
@@ -20,53 +20,53 @@ if /i "%PackageName%" NEQ "" (
     echo:
     echo [Creating NuGet Package: %PackageName% Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\%PackageName%.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\%PackageName%.nuspec && echo: || Goto :Error
 
 ) else (
     echo:
     echo [Creating NuGet Package: DiskSpd Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\diskspd.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\diskspd.nuspec && echo: || Goto :Error
     
     echo:
     echo [Creating NuGet Package: FIO Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\fio.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\fio.nuspec && echo: || Goto :Error
 
     echo:
     echo [Creating NuGet Package: Graph500 Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\graph500.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\graph500.nuspec && echo: || Goto :Error
 
     echo:
     echo [Creating NuGet Package: LAPACK Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\lapack.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\lapack.nuspec && echo: || Goto :Error
     
     echo:
     echo [Creating NuGet Package: Networking Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\networking.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\networking.nuspec && echo: || Goto :Error
 
     echo:
     echo [Creating NuGet Package: OpenFoam Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\openfoam.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\openfoam.nuspec && echo: || Goto :Error
 
     echo:
     echo [Creating NuGet Package: OpenSSL Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\openssl.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\openssl.nuspec && echo: || Goto :Error
 
     echo:
     echo [Creating NuGet Package: PowerShell7 Runtime]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\powershell7.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\powershell7.nuspec && echo: || Goto :Error
 
     echo:
     echo [Creating NuGet Package: LMbench Workload]
     echo --------------------------------------------------
-    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Debug -p:NuspecFile=%PackagingDir%\nuspec\lmbench.nuspec && echo: || Goto :Error
+    call dotnet pack %PackagingProject% --force --no-restore --no-build -c Release -p:NuspecFile=%PackagingDir%\nuspec\lmbench.nuspec && echo: || Goto :Error
 )
 
 Goto :End
