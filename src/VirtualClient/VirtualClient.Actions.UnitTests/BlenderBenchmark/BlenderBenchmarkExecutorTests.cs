47e221fbba45d17d08bf256e666b9785208da20c
James Li <yueqianlijames@outlook.com>
2023-11-18 00:53:42 +0000

    blender unit tests

---
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/BlenderBenchmark/BlenderBenchmarkExecutorTests.cs
@@ -0,0 +1,150 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using Moq;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    [TestFixture]
+    [Category("Unit")]
+    public class BlenderBenchmarkExecutorTests
+    {
+        private MockFixture mockFixture;
+        private DependencyPath mockBlenderPackage;
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            this.mockFixture = new MockFixture();
+        }
+
+        [Test]
+        [TestCase(PlatformID.Win32NT, Architecture.X64)]
+        public async Task BlenderExecutorInitializesItsDependenciesAsExpected(PlatformID platform, Architecture architecture)
+        {
+            this.SetupMockBehavior(platform, architecture);
+            using (TestBlenderExecutor executor = new TestBlenderExecutor(this.mockFixture))
+            {
+                this.mockFixture.ProcessManager.OnCreateProcess = (command, arguments, workingDirectory) =>
+                {
+                    return this.mockFixture.Process;
+                };
+                await executor.InitializeAsync(EventContext.None, CancellationToken.None)
+                    .ConfigureAwait(false);
+
+                string expectedExecutablePath = this.mockFixture.PlatformSpecifics.Combine(
+                    this.mockFixture.ToPlatformSpecificPath(this.mockBlenderPackage, this.mockFixture.Platform, this.mockFixture.CpuArchitecture).Path, "benchmark-launcher-cli.exe");
+
+                Assert.AreEqual(expectedExecutablePath, executor.ExecutablePath);
+            }
+        }
+
+        [Test]
+        [TestCase(PlatformID.Win32NT, Architecture.X64)]
+        public async Task BlenderExecutorExecutesWorkloadAsExpectedJunkshopHIP(PlatformID platform, Architecture architecture)
+        {
+            this.SetupMockBehavior(platform, architecture, "junkshop", "HIP", "junkshop_hip.json");
+            await this.BlenderExecutorExecutesWorkloadAsExpected(platform, architecture);
+        }
+
+
+        [Test]
+        [TestCase(PlatformID.Win32NT, Architecture.X64)]
+        public async Task BlenderExecutorExecutesWorkloadAsExpectedMonsterCPU(PlatformID platform, Architecture architecture)
+        {
+            this.SetupMockBehavior(platform, architecture, "monster", "CPU", "monster_cpu.json");
+            await this.BlenderExecutorExecutesWorkloadAsExpected(platform, architecture);
+        }
+
+        private async Task BlenderExecutorExecutesWorkloadAsExpected(PlatformID platform, Architecture architecture)
+        {
+            using (TestBlenderExecutor executor = new TestBlenderExecutor(this.mockFixture))
+            {
+                int executed = 0, index = 0;
+
+                // Blender will execute twice, each time benchmarking on one device type.
+                string[] deviceTypes = this.mockFixture.Parameters["DeviceTypes"].ToString().Split(',', StringSplitOptions.TrimEntries);
+
+                if (platform == PlatformID.Win32NT)
+                {
+                    string[] commandArgumentsArray = new string[deviceTypes.Length];
+                    string commandArgument;
+                    for (int i = 0; i < deviceTypes.Length; i++)
+                    {
+                        commandArgument = $"benchmark --blender-version {this.mockFixture.Parameters["BlenderVersion"]} --device-type {deviceTypes[i]} {this.mockFixture.Parameters["Scenes"]} --json --verbosity 3";
+                        commandArgumentsArray[i] = commandArgument;
+                    }
+
+                    string expectedExecutablePath = this.mockFixture.PlatformSpecifics.Combine(
+                        this.mockFixture.ToPlatformSpecificPath(this.mockBlenderPackage, this.mockFixture.Platform, this.mockFixture.CpuArchitecture).Path, "benchmark-launcher-cli.exe");
+                    string workingDir = this.mockBlenderPackage.Path;
+
+                    this.mockFixture.ProcessManager.OnCreateProcess = (command, arguments, workingDirectory) =>
+                    {
+                        if (arguments == commandArgumentsArray[index] && command == expectedExecutablePath)
+                        {
+                            executed++;
+                            index++; // If Blender needs to benchmark on another device, the increase index result in the next set of command and arguments being used.
+                        }
+
+                        return this.mockFixture.Process;
+                    };
+
+                    await executor.ExecuteAsync(EventContext.None, CancellationToken.None);
+                }
+
+                Assert.AreEqual(deviceTypes.Length, executed);
+            }
+        }
+
+        private void SetupMockBehavior(PlatformID platform = PlatformID.Win32NT, Architecture architecture = Architecture.X64, string scenes = "", string devicetypes = "", string exampleResultsFilePath = "")
+        {
+            this.mockFixture.Setup(platform, architecture);
+
+            this.mockFixture.Parameters = new Dictionary<string, IConvertible>
+            {
+                {"Scenario", "BlenderbenchmarkUnitTest"},
+                {"PackageName", "blenderbenchmarkcli"},
+                {"BlenderVersion", "3.6.0"},
+                {"Scenes", scenes},
+                {"DeviceTypes", devicetypes}
+            };
+
+            this.mockBlenderPackage = new DependencyPath("blenderbenchmarkcli", this.mockFixture.GetPackagePath("blenderbenchmarkcli"));
+            this.mockFixture.PackageManager.OnGetPackage("blenderbenchmarkcli").ReturnsAsync(this.mockBlenderPackage);
+            this.mockFixture.ProcessManager.OnCreateProcess = (command, arguments, directory) => this.mockFixture.Process;
+            string results = File.ReadAllText(Path.Combine(MockFixture.ExamplesDirectory, "BlenderBenchmark", exampleResultsFilePath));
+            this.mockFixture.Process.StandardOutput = new Common.ConcurrentBuffer(new StringBuilder(results));
+        }
+
+        private class TestBlenderExecutor : BlenderBenchmarkExecutor
+        {
+            public TestBlenderExecutor(MockFixture fixture)
+                : base(fixture.Dependencies, fixture.Parameters)
+            {
+            }
+            public new string ExecutablePath => base.ExecutablePath;
+
+            public new Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+            {
+                return base.InitializeAsync(telemetryContext, cancellationToken);
+            }
+
+            public new Task ExecuteAsync(EventContext context, CancellationToken cancellationToken)
+            {
+                this.InitializeAsync(context, cancellationToken).GetAwaiter().GetResult();
+                return base.ExecuteAsync(context, cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/BlenderBenchmark/BlenderBenchmarkMetricsParserTests.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+    using VirtualClient.Contracts;
+
+    [TestFixture]
+    [Category("Unit")]
+    public class BlenderBenchmarkMetricsParserTests
+    {
+        private string ExamplePath
+        {
+            get
+            {
+                string workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                return Path.Combine(workingDirectory, "Examples", "BlenderBenchmark");
+            }
+        }
+
+        private IList<Metric> actualMetrics { get; set; }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            string outputPath = Path.Combine(ExamplePath, "monster_cpu.json");
+            string rawText = File.ReadAllText(outputPath);
+
+            BlenderBenchmarkMetricsParser testParser = new BlenderBenchmarkMetricsParser(rawText);
+            this.actualMonsterMetrics = testParser.Parse();
+        }
+
+
+        [Test]
+        public void BlenderMetricsParserTestsCorrectly_Monster()
+        {
+            MetricAssert.Exists(this.actualMetrics, "device_peak_memory", 3113.97, "mb");
+            MetricAssert.Exists(this.actualMetrics, "number_of_samples", 432, "sample");
+            MetricAssert.Exists(this.actualMetrics, "time_for_samples", 30.0464, "second");
+            MetricAssert.Exists(this.actualMetrics, "samples_per_minute", 8931.312909031, "samples_per_minute");
+            MetricAssert.Exists(this.actualMetrics, "total_render_time", 31.8678, "second");
+            MetricAssert.Exists(this.actualMetrics, "render_time_no_sync", 30.0468, "second");
+        }
+
+        [Test]
+        public void BlenderMetricsParserTestsCorrectly_JunkShop()
+        {
+            MetricAssert.Exists(this.actualMetrics, "device_peak_memory", 789.97, "mb");
+            MetricAssert.Exists(this.actualMetrics, "number_of_samples", 793, "sample");
+            MetricAssert.Exists(this.actualMetrics, "time_for_samples", 30.142288, "second");
+            MetricAssert.Exists(this.actualMetrics, "samples_per_minute", 3791.312909031, "samples_per_minute");
+            MetricAssert.Exists(this.actualMetrics, "total_render_time", 38.0937, "second");
+            MetricAssert.Exists(this.actualMetrics, "render_time_no_sync", 30.143, "second");
+        }
+
+        [Test]
+        public void BlenderMetricsParserTestsCorrectly_Classroom()
+        {
+            MetricAssert.Exists(this.actualMetrics, "device_peak_memory", 431.123, "mb");
+            MetricAssert.Exists(this.actualMetrics, "number_of_samples", 72, "sample");
+            MetricAssert.Exists(this.actualMetrics, "time_for_samples", 30.893534, "second");
+            MetricAssert.Exists(this.actualMetrics, "samples_per_minute", 673.312909031, "samples_per_minute");
+            MetricAssert.Exists(this.actualMetrics, "total_render_time", 31.7936, "second");
+            MetricAssert.Exists(this.actualMetrics, "render_time_no_sync", 30.0468, "second");
+        }
+
+        [Test]
+        public void BlenderMetricsParserTestsCorrectly_Metadata_Scene()
+        {
+            foreach (Metric metric in this.actualMetrics)
+            {
+                // Asserts that the scene name is one of "monster", "junkshop", "classroom"
+                Assert.Contains(metric.Metadata["scene"], new List<string> { "monster", "junkshop", "classroom" });
+            }
+        }
+
+
+        [Test]
+        public void BlenderMetricsParserTestsCorrectly_Metadata_Others()
+        {
+            // These metadata are constants.
+            var expectedMetadata = new Dictionary<string, IConvertible>
+                {
+                    {"blenderVersion", "3.6.0"},
+                    {"benchmarkLauncher", "3.1.0"},
+                    {"deviceName", "AMD Radeon Pro V620 MxGPU"},
+                    {"deviceType", "HIP"},
+                    {"timeLimit", 30}
+                };
+
+            IDictionary<string, IConvertible> actualMetadataCopy;
+            foreach (Metric metric in this.actualMetrics)
+            {
+                actualMetadataCopy = new Dictionary<string, IConvertible>(metric.Metadata);
+
+                // Remove scenes as it is not constant and is tested elsewhere.
+                actualMetadataCopy.Remove("scene");
+                Assert.AreEqual(expectedMetadata, actualMetadataCopy);
+            }
+        }
+    }
+}
