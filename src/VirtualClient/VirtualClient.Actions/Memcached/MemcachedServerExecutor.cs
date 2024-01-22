d2455d778593590f9828120d548ab027e338f26a
nikhitha malkapuram <nmalkapuram@microsoft.com>
2024-01-22 17:40:28 +0530

    removing unused library reference

--- src/VirtualClient/VirtualClient.Actions/Memcached/MemcachedServerExecutor.cs
+++ src/VirtualClient/VirtualClient.Actions/Memcached/MemcachedServerExecutor.cs
@@ -5,7 +5,6 @@ namespace VirtualClient.Actions
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
