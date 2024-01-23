8d1a9f81696dd871340860fdccae37832ea986b8
Aditya Abhishek <adityaa@microsoft.com>
2024-01-23 14:49:24 +0530

    add executor and parser

---
+++ src/VirtualClient/VirtualClient.Actions/ScriptExecutor/JsonMetricsParser.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace CRC.Toolkit.VirtualClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using global::VirtualClient;
+    using global::VirtualClient.Actions;
+    using global::VirtualClient.Contracts;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Generic parser for JSON results log.
+    /// </summary>
+    internal class JsonMetricsParser : MetricsParser
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonMetricsParser"/> class.
+        /// </summary>
+        /// <param name="results">The generic script results in JSON format.</param>
+        public JsonMetricsParser(string results)
+            : base(results)
+        {
+        }
+
+        /// <summary>
+        /// Parses key metrics from the JSON based output log.
+        /// </summary>
+        public override IList<Metric> Parse()
+        {
+            List<Metric> metrics = new List<Metric>();
+            try
+            {
+                JObject keyValuePairs = JObject.Parse(this.RawText);
+                foreach (var keyValuePair in keyValuePairs.Properties())
+                {
+                    if (double.TryParse(keyValuePair.Value.ToString(), out var value))
+                    {
+                        metrics.Add(new Metric(keyValuePair.Name, value, MetricRelativity.Undefined));
+                    }
+                    else
+                    {
+                        throw new WarningException($"The metric value for {keyValuePair.Name} couldn't be parsed, it should be of Double type.");
+                    }
+                }
+            }
+            catch
+            {
+                throw new SchemaException("The log File has incorrect JSON format. The log file should have metric name as keys and metricValue as Value in JSON format");
+            }
+
+            return metrics;
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Actions/ScriptExecutor/ScriptExecutor.cs
@@ -0,0 +1,259 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Abstractions;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using CRC.Toolkit.VirtualClient;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.Extensions.DependencyInjection;
+    using VirtualClient.Common;
+    using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+    using VirtualClient.Contracts.Metadata;
+
+    /// <summary>
+    /// The Generic Script executor for Python, Powershell, etc scripts.
+    /// </summary>
+    public class ScriptExecutor : VirtualClientComponent
+    {
+        private IFileSystem fileSystem;
+        private IPackageManager packageManager;
+        private ISystemManagement systemManagement;
+        private List<int> successExitCodes;
+
+        /// <summary>
+        /// Constructor for <see cref="ScriptExecutor"/>
+        /// </summary>
+        /// <param name="dependencies">Provides required dependencies to the component.</param>
+        /// <param name="parameters">Parameters defined in the profile or supplied on the command line.</param>
+        public ScriptExecutor(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
+             : base(dependencies, parameters)
+        {
+            this.systemManagement = this.Dependencies.GetService<ISystemManagement>();
+            this.packageManager = this.systemManagement.PackageManager;
+            this.fileSystem = this.systemManagement.FileSystem;
+        }
+
+        /// <summary>
+        /// The commandline arguments to be used with the Script executable
+        /// </summary>
+        public string CommandLine
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(this.CommandLine));
+            }
+        }
+
+        /// <summary>
+        /// The relative Executable Path to be used to initiate the script
+        /// </summary>
+        public string Executable
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(this.Executable));
+            }
+        }
+
+        /// <summary>
+        /// The Regex Based, semi-colon separated relative log file/Folder Paths
+        /// </summary>
+        public string LogPaths
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(this.LogPaths));
+            }
+        }
+
+        /// <summary>
+        /// The parameter specifies whether to run in Elevated Mode
+        /// </summary>
+        public bool RunElevated
+        {
+            get
+            {
+                return this.Parameters.GetValue<bool>(nameof(this.RunElevated));
+            }
+        }
+
+        /// <summary>
+        /// The ToolName for better logging and metadata
+        /// </summary>
+        public string ToolName
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(this.ToolName));
+            }
+        }
+
+        /// <summary>
+        /// The full path to the script executable.
+        /// </summary>
+        public string ExecutablePath { get; set; }
+
+        /// <summary>
+        /// The path to the workload package.
+        /// </summary>
+        protected DependencyPath WorkloadPackage { get; private set; }
+
+        /// <summary>
+        /// Initializes the environment for execution of the provided script.
+        /// </summary>
+        protected override async Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            await this.EvaluateParametersAsync(cancellationToken);
+
+            this.WorkloadPackage = await this.GetPlatformSpecificPackageAsync(this.PackageName, cancellationToken);
+
+            this.ExecutablePath = this.Combine(this.WorkloadPackage.Path, this.Executable);
+
+            await this.systemManagement.MakeFileExecutableAsync(this.ExecutablePath, this.Platform, cancellationToken);
+
+            if (!this.fileSystem.File.Exists(this.ExecutablePath))
+            {
+                throw new DependencyException(
+                    $"The expected workload binary/executable was not found in the '{this.PackageName}' package. The script cannot be executed " +
+                    $"successfully without this binary/executable. Check that the workload package was installed successfully and that the executable " +
+                    $"exists in the path expected '{this.ExecutablePath}'.",
+                    ErrorReason.DependencyNotFound);
+            }
+        }
+
+        /// <summary>
+        /// Executes the workload.
+        /// </summary>
+        protected override async Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            using (BackgroundOperations profiling = BackgroundOperations.BeginProfiling(this, cancellationToken))
+            {
+                using (IProcessProxy process = await this.ExecuteCommandAsync(
+                    this.ExecutablePath,
+                    this.CommandLine,
+                    this.WorkloadPackage.Path,
+                    telemetryContext,
+                    cancellationToken,
+                    this.RunElevated))
+                {
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        await this.LogProcessDetailsAsync(process, telemetryContext, this.Scenario, logToFile: true);
+                        process.ThrowIfWorkloadFailed();
+                        await this.CaptureMetricsAsync(process, telemetryContext, cancellationToken);
+                        await this.CaptureLogsAsync(cancellationToken);
+                    }
+                }
+            }
+        }        
+
+        /// <summary>
+        /// Captures the workload logs and the Workload metrics.
+        /// </summary>
+        private async Task CaptureMetricsAsync(IProcessProxy process, EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                this.MetadataContract.AddForScenario(
+                    this.Scenario,
+                    process.FullCommand());
+
+                this.MetadataContract.Apply(telemetryContext);
+
+                string metricsFilePath = this.Combine(this.WorkloadPackage.Path, "test-metrics.json");
+                string results = await this.fileSystem.File.ReadAllTextAsync(metricsFilePath);
+
+                JsonMetricsParser parser = new JsonMetricsParser(results);
+                IList<Metric> workloadMetrics = parser.Parse();
+
+                this.Logger.LogMetrics(
+                    this.ToolName,
+                    this.Scenario,
+                    process.StartTime,
+                    process.ExitTime,
+                    workloadMetrics,
+                    null,
+                    process.FullCommand(),
+                    this.Tags,
+                    telemetryContext);
+            }
+        }
+
+        private async Task CaptureLogsAsync(CancellationToken cancellationToken)
+        {
+            string destinitionLogsDir = this.Combine(this.PlatformSpecifics.LogsDirectory, this.ToolName, $"{this.Scenario}_{DateTime.UtcNow.ToString()}");
+            if (!this.fileSystem.Directory.Exists(destinitionLogsDir))
+            {
+                this.fileSystem.Directory.CreateDirectory(destinitionLogsDir);
+            }
+
+            foreach (string logPath in this.LogPaths.Split(";"))
+            {
+                string fullLogPath = this.Combine(this.WorkloadPackage.Path, logPath);
+
+                // Check for Matching Sub-Directories 
+                if (this.fileSystem.Directory.Exists(fullLogPath))
+                {
+                    foreach (string logFilePath in this.fileSystem.Directory.GetFiles(fullLogPath, "*", SearchOption.AllDirectories))
+                    {
+                        if (this.TryGetContentStoreManager(out IBlobManager blobManager))
+                        {
+                            await this.UploadLogAsync(blobManager, logFilePath, cancellationToken);
+                        }
+
+                        this.MoveFileToCentralLogsDirectory(logFilePath, destinitionLogsDir);
+                    }
+                }
+
+                // Check for Matching FileNames
+                foreach (string logFilePath in this.fileSystem.Directory.GetFiles(this.WorkloadPackage.Path, logPath, SearchOption.AllDirectories))
+                {
+                    if (this.TryGetContentStoreManager(out IBlobManager blobManager))
+                    {
+                        await this.UploadLogAsync(blobManager, logFilePath, cancellationToken);
+                    }
+
+                    this.MoveFileToCentralLogsDirectory(logFilePath, destinitionLogsDir);
+                }
+            }
+        }
+
+        private Task UploadLogAsync(IBlobManager blobManager, string logPath, CancellationToken cancellationToken)
+        {
+            FileUploadDescriptor descriptor = this.CreateFileUploadDescriptor(
+                new FileContext(
+                    this.fileSystem.FileInfo.New(logPath),
+                    HttpContentType.PlainText,
+                    Encoding.UTF8.WebName,
+                    this.ExperimentId,
+                    this.AgentId,
+                    this.ToolName,
+                    this.Scenario,
+                    null,
+                    this.Roles?.FirstOrDefault()));
+
+            return this.UploadFileAsync(blobManager, this.fileSystem, descriptor, cancellationToken, deleteFile: false);
+        }
+
+        private void MoveFileToCentralLogsDirectory(string sourcePath, string destinitionDirectory)
+        {
+            string fileName = Path.GetFileName(sourcePath);
+
+            string destinitionPath = this.Combine(destinitionDirectory, fileName);
+            this.fileSystem.File.Move(sourcePath, destinitionPath, true);
+        }
+    }
+}
