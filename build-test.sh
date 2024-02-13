d5db9bbfd057653514aeea1a9d52c4b394cfebc3
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2024-02-12 17:42:51 -0800

    test

--- build-test.sh
+++ build-test.sh
@@ -33,7 +33,8 @@ echo "[Running Tests]"
 echo "--------------------------------------------------"
 
 for file in $(find "$(dirname "$0")/src" -type f -name "*Tests.csproj"); do
-    dotnet test -c Release "$file" --no-restore --no-build --filter "(Category=Unit|Category=Functional)" --logger "console;verbosity=normal"
+    test_project="${file%.*}.dll"  # Change the file extension from .csproj to .dll
+    dotnet test -c Release "$test_project" --no-restore --no-build --filter "(Category=Unit|Category=Functional)" --logger "console;verbosity=normal"
     result=$?
     if [ $result -ne 0 ]; then
         Error
