5d1395731cf4317a85a6a59b987c4987a28b579f
Kayvon Tadj <kayvontadj@microsoft.com>
2024-03-07 13:48:56 -0800

    updated server configuration

--- src/VirtualClient/VirtualClient.Dependencies/PostgreSQLServer/PostgreSQLServerConfiguration.cs
+++ src/VirtualClient/VirtualClient.Dependencies/PostgreSQLServer/PostgreSQLServerConfiguration.cs
@@ -5,6 +5,7 @@ namespace VirtualClient.Dependencies
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using MathNet.Numerics.Distributions;
@@ -17,6 +18,8 @@ namespace VirtualClient.Dependencies
     using VirtualClient.Common.Platform;
     using VirtualClient.Common.Telemetry;
     using VirtualClient.Contracts;
+    using VirtualClient.Dependencies.MySqlServer;
+    using static VirtualClient.Dependencies.MySqlServer.MySQLServerConfiguration;
 
     /// <summary>
     /// Provides functionality for configuring PostgreSQL Server.
@@ -25,22 +28,68 @@ namespace VirtualClient.Dependencies
     [WindowsCompatible]
     public class PostgreSQLServerConfiguration : ExecuteCommand
     {
-        private ISystemManagement systemManager;
-        private IPackageManager packageManager;
-        private IStateManager stateManager;
+        private const string PythonCommand = "python3";
+        private readonly IStateManager stateManager;
+        private string packageDirectory;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PostgreSQLServerConfiguration"/> class.
+        /// Initializes a new instance of the <see cref="MySQLServerConfiguration"/> class.
         /// </summary>
         /// <param name="dependencies">An enumeration of dependencies that can be used for dependency injection.</param>
         /// <param name="parameters">A series of key value pairs that dictate runtime execution.</param>
         public PostgreSQLServerConfiguration(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
             : base(dependencies, parameters)
         {
-            this.systemManager = dependencies.GetService<ISystemManagement>();
-            this.systemManager.ThrowIfNull(nameof(this.systemManager));
-            this.packageManager = this.systemManager.PackageManager;
-            this.stateManager = this.systemManager.StateManager;
+            dependencies.ThrowIfNull(nameof(dependencies));
+
+            this.SystemManager = dependencies.GetService<ISystemManagement>();
+            this.SystemManager.ThrowIfNull(nameof(this.SystemManager));
+            this.stateManager = this.SystemManager.StateManager;
+        }
+
+        /// <summary>
+        /// The specifed action that controls the execution of the dependency.
+        /// </summary>
+        public string Action
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(this.Action));
+            }
+        }
+
+        /// <summary>
+        /// The specifed action that controls the execution of the dependency.
+        /// </summary>
+        public bool SkipInitialize
+        {
+            get
+            {
+                return this.Parameters.GetValue<bool>(nameof(this.SkipInitialize), false);
+            }
+        }
+
+        /// <summary>
+        /// Disk filter specified
+        /// </summary>
+        public string DiskFilter
+        {
+            get
+            {
+                // and 256G
+                return this.Parameters.GetValue<string>(nameof(this.DiskFilter), "osdisk:false&sizegreaterthan:256g");
+            }
+        }
+
+        /// <summary>
+        /// Database Name to create and utilize
+        /// </summary>
+        public string DatabaseName
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(this.DatabaseName), "vc-mysqldb");
+            }
         }
 
         /// <summary>
@@ -56,21 +105,31 @@ namespace VirtualClient.Dependencies
         }
 
         /// <summary>
-        /// The path to the PostgreSQL package for configuration.
+        /// Parameter defines the SuperUser Password for PostgreSQL Server.
         /// </summary>
-        protected string PackagePath { get; set; }
+        public int SuperUserPassword
+        {
+            get
+            {
+                return this.ExperimentId.GetHashCode();
+            }
+        }
 
         /// <summary>
-        /// Initializes PostgreSQL configuration requirements.
+        /// Denotes if In-Memory scenario will be utilized
         /// </summary>
-        /// <param name="telemetryContext">Provides context information that will be captured with telemetry events.</param>
-        /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
-        /// <returns></returns>
-        protected override async Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        public bool InMemory
         {
-            DependencyPath package = await this.GetPlatformSpecificPackageAsync(this.PackageName, cancellationToken);
-            this.PackagePath = package.Path;
+            get
+            {
+                return this.Parameters.GetValue<bool>(nameof(this.InMemory), false);
+            }
         }
+
+        /// <summary>
+        /// Retrieves the interface to interacting with the underlying system.
+        /// </summary>
+        protected ISystemManagement SystemManager { get; }
 
         /// <summary>
         /// Executes PostgreSQL configuration steps.
@@ -79,65 +138,189 @@ namespace VirtualClient.Dependencies
         /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
         protected override async Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
         {
-            ConfigurationState state = await this.stateManager.GetStateAsync<ConfigurationState>(nameof(PostgreSQLServerConfiguration), cancellationToken);
+            ProcessManager manager = this.SystemManager.ProcessManager;
+            string stateId = $"{nameof(PostgreSQLServerConfiguration)}-{this.Action}-action-success";
+            ConfigurationState configurationState = await this.stateManager.GetStateAsync<ConfigurationState>($"{nameof(ConfigurationState)}", cancellationToken)
+                .ConfigureAwait(false);
 
-            if (state == null)
+            DependencyPath workloadPackage = await this.GetPackageAsync(this.PackageName, cancellationToken).ConfigureAwait(false);
+            workloadPackage.ThrowIfNull(this.PackageName);
+
+            DependencyPath package = await this.GetPlatformSpecificPackageAsync(this.PackageName, cancellationToken);
+            this.packageDirectory = package.Path;
+
+            telemetryContext.AddContext(nameof(configurationState), configurationState);
+
+            if (!this.SkipInitialize)
             {
-                await this.ConfigureServerAsync(telemetryContext, cancellationToken);
+                if (configurationState == null)
+                {
+                    switch (this.Action)
+                    {
+                        case ConfigurationAction.ConfigureServer:
+                            await this.ConfigurePostgreSQLServerAsync(telemetryContext, cancellationToken)
+                                .ConfigureAwait(false);
+                            break;
+                        case ConfigurationAction.CreateDatabase:
+                            await this.CreatePostgreSQLServerDatabaseAsync(telemetryContext, cancellationToken)
+                                .ConfigureAwait(false);
+                            break;
+                        case ConfigurationAction.DistributeDatabase:
+                            await this.DistributeMySQLDatabaseAsync(telemetryContext, cancellationToken)
+                                .ConfigureAwait(false);
+                            break;
+                    }
 
-                await this.stateManager.SaveStateAsync(
-                    nameof(PostgreSQLServerConfiguration),
-                    new Item<ConfigurationState>(nameof(PostgreSQLServerConfiguration), new ConfigurationState()),
-                    cancellationToken);
+                    await this.stateManager.SaveStateAsync(stateId, new ConfigurationState(this.Action), cancellationToken);
+                }
             }
         }
 
-        private async Task ConfigureServerAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        private async Task ConfigurePostgreSQLServerAsync(EventContext telemetryContext, CancellationToken cancellationToken)
         {
-            string configurationScript = this.Combine(this.PackagePath, "configure-server.py");
+            string arguments = $"{this.packageDirectory}/configure-server.py --port {this.Port}";
 
-            string command = "python3";
-            string commandArguments = $"{configurationScript} --port {this.Port}";
-            string workingDirectory = this.Combine(this.PackagePath);
-
-            EventContext relatedContext = telemetryContext.Clone()
-                .AddContext("command", command)
-                .AddContext("commandArguments", commandArguments)
-                .AddContext("workingDirectory", workingDirectory);
-
-            await this.systemManager.MakeFileExecutableAsync(configurationScript, this.Platform, cancellationToken);
-
-            await this.RetryPolicy.ExecuteAsync(async () =>
+            if (this.InMemory)
             {
-                using (IProcessProxy process = await this.ExecuteCommandAsync(command, commandArguments, workingDirectory, relatedContext, cancellationToken, runElevated: true))
+                string inMemoryMB = await this.GetPostgreSQLInMemoryCapacityAsync(cancellationToken);
+                arguments += $" --inMemory {inMemoryMB}";
+            }
+
+            using (IProcessProxy process = await this.ExecuteCommandAsync(
+               "python3",
+               arguments,
+               Environment.CurrentDirectory,
+               telemetryContext,
+               cancellationToken))
+            {
+                if (!cancellationToken.IsCancellationRequested)
                 {
-                    if (!cancellationToken.IsCancellationRequested)
+                    await this.LogProcessDetailsAsync(process, telemetryContext, "PostgreSQLServerConfiguration", logToFile: true);
+                    process.ThrowIfDependencyInstallationFailed(process.StandardError.ToString());
+                }
+            }
+        }
+
+        private async Task CreatePostgreSQLServerDatabaseAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            string arguments = $"{this.packageDirectory}/setup-database.py --dbName {this.DatabaseName} --password {this.SuperUserPassword}";
+
+            using (IProcessProxy process = await this.ExecuteCommandAsync(
+                PythonCommand,
+                arguments,
+                Environment.CurrentDirectory,
+                telemetryContext,
+                cancellationToken))
+            {
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    await this.LogProcessDetailsAsync(process, telemetryContext, "MySQLServerConfiguration", logToFile: true);
+                    process.ThrowIfDependencyInstallationFailed(process.StandardError.ToString());
+                }
+            }
+        }
+
+        private async Task DistributeMySQLDatabaseAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            string innoDbDirs = await this.GetPostgreSQLInnodbDirectoriesAsync(cancellationToken);
+
+            string arguments = $"{this.packageDirectory}/distribute-database.py --dbName {this.DatabaseName} --directories {innoDbDirs}";
+
+            using (IProcessProxy process = await this.ExecuteCommandAsync(
+                    PythonCommand,
+                    arguments,
+                    Environment.CurrentDirectory,
+                    telemetryContext,
+                    cancellationToken))
+            {
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    await this.LogProcessDetailsAsync(process, telemetryContext, "PostgreSQLServerConfiguration", logToFile: true);
+                    process.ThrowIfDependencyInstallationFailed(process.StandardError.ToString());
+                }
+            }
+        }
+
+        private async Task<string> GetPostgreSQLInnodbDirectoriesAsync(CancellationToken cancellationToken)
+        {
+            string diskPaths = string.Empty;
+
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                IEnumerable<Disk> disks = await this.SystemManager.DiskManager.GetDisksAsync(cancellationToken)
+                        .ConfigureAwait(false);
+
+                if (disks?.Any() != true)
+                {
+                    throw new WorkloadException(
+                        "Unexpected scenario. The disks defined for the system could not be properly enumerated.",
+                        ErrorReason.WorkloadUnexpectedAnomaly);
+                }
+
+                IEnumerable<Disk> disksToTest = DiskFilters.FilterDisks(disks, this.DiskFilter, this.Platform).ToList();
+
+                if (disksToTest?.Any() != true)
+                {
+                    throw new WorkloadException(
+                        "Expected disks to test not found. Given the parameters defined for the profile action/step or those passed " +
+                        "in on the command line, the requisite disks do not exist on the system or could not be identified based on the properties " +
+                        "of the existing disks.",
+                        ErrorReason.DependencyNotFound);
+                }
+
+                foreach (Disk disk in disksToTest)
+                {
+                    if (disk.GetPreferredAccessPath(this.Platform) != "/mnt")
                     {
-                        await this.LogProcessDetailsAsync(process, relatedContext, logToFile: true);
-                        process.ThrowIfDependencyInstallationFailed();
+                        diskPaths += $"{disk.GetPreferredAccessPath(this.Platform)};";
                     }
                 }
-            });
+            }
+
+            return diskPaths;
         }
 
-        internal class ConfigurationState : State
+        private async Task<string> GetPostgreSQLInMemoryCapacityAsync(CancellationToken cancellationToken)
+        {
+            MemoryInfo memoryInfo = await this.SystemManager.GetMemoryInfoAsync(cancellationToken);
+            long totalMemoryKiloBytes = memoryInfo.TotalMemory;
+            int bufferSizeInMegaBytes = Convert.ToInt32(totalMemoryKiloBytes / 1024); 
+
+            return bufferSizeInMegaBytes.ToString();
+        }
+
+        /// <summary>
+        /// Supported PostgreSQL Server configuration actions.
+        /// </summary>
+        internal class ConfigurationAction
         {
             /// <summary>
-            /// Initializes a new instance of the <see cref="ConfigurationState"/> object.
+            /// Setup the required configurations of the SQL Server.
             /// </summary>
-            public ConfigurationState()
-                : base()
-            {
-            }
+            public const string ConfigureServer = nameof(ConfigureServer);
 
             /// <summary>
-            /// Initializes a new instance of the <see cref="ConfigurationState"/> object.
+            /// Creates Database on PostgreSQL server and Users on Server and any Clients.
             /// </summary>
+            public const string CreateDatabase = nameof(CreateDatabase);
+
+            /// <summary>
+            /// Distributes existing database to disks on the system
+            /// </summary>
+            public const string DistributeDatabase = nameof(DistributeDatabase);
+
+        }
+
+        internal class ConfigurationState
+        {
             [JsonConstructor]
-            public ConfigurationState(IDictionary<string, IConvertible> properties = null)
-                : base(properties)
+            public ConfigurationState(string action)
             {
+                this.Action = action;
             }
+
+            [JsonProperty("action")]
+            public string Action { get; }
         }
     }
 }
