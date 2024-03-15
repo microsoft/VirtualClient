1b5dea2ce37922ddf01b9aa5a85aeafa7e3670d4
Prachi Singla <prachisingla@microsoft.com>
2024-03-15 20:06:01 +0530

    Improving parser

--- src/VirtualClient/VirtualClient.Monitors/NvidiaSmiQueryGpuParser.cs
+++ src/VirtualClient/VirtualClient.Monitors/NvidiaSmiQueryGpuParser.cs
@@ -44,48 +44,53 @@ namespace VirtualClient.Monitors
             {
                 Dictionary<string, IConvertible> metadata = new Dictionary<string, IConvertible>()
                 {
-                    { "name", Convert.ToString(row[1]) },
-                    { "pci.bus_id", Convert.ToString(row[2]) },
-                    { "driver_version", Convert.ToString(row[3]) },
-                    { "pstate", Convert.ToString(row[4]) },
-                    { "pcie.link.gen.max", Convert.ToString(row[5]) },
-                    { "pcie.link.gen.current", Convert.ToString(row[6]) }
+                    { "name", SafeGet(row, "name") },
+                    { "pci.bus_id", SafeGet(row, "pci.bus_id") },
+                    { "driver_version", SafeGet(row, "driver_version") },
+                    { "pstate", SafeGet(row, "pstate") },
+                    { "pcie.link.gen.max", SafeGet(row, "pcie.link.gen.max") },
+                    { "pcie.link.gen.current", SafeGet(row, "pcie.link.gen.current") }
                 };
 
-                metrics.Add(new Metric("utilization.gpu", Convert.ToDouble(row[7]), unit: "%", metadata: metadata));
-                metrics.Add(new Metric("utilization.memory", Convert.ToDouble(row[8]), unit: "%", metadata: metadata));
-                metrics.Add(new Metric("temperature.gpu", Convert.ToDouble(row[9]), unit: "celsuis", metadata: metadata));
-                metrics.Add(new Metric("temperature.memory", Convert.ToDouble(row[10]), unit: "celsuis", metadata: metadata));
-                metrics.Add(new Metric("power.draw.average", Convert.ToDouble(row[11]), unit: "W", metadata: metadata));
-                metrics.Add(new Metric("clocks.gr", Convert.ToDouble(row[12]), unit: "MHz", metadata: metadata));
-                metrics.Add(new Metric("clocks.sm", Convert.ToDouble(row[13]), unit: "MHz", metadata: metadata));
-                metrics.Add(new Metric("clocks.video", Convert.ToDouble(row[14]), unit: "MHz", metadata: metadata));
-                metrics.Add(new Metric("clocks.mem", Convert.ToDouble(row[15]), unit: "MHz", metadata: metadata));
-                metrics.Add(new Metric("memory.total", Convert.ToDouble(row[16]), unit: "MiB", metadata: metadata));
-                metrics.Add(new Metric("memory.free", Convert.ToDouble(row[17]), unit: "MiB", metadata: metadata));
-                metrics.Add(new Metric("memory.used", Convert.ToDouble(row[18]), unit: "MiB", metadata: metadata));
-                metrics.Add(new Metric("power.draw.instant", Convert.ToDouble(row[19]), unit: "W", metadata: metadata));
-                metrics.Add(new Metric("pcie.link.gen.gpucurrent", Convert.ToDouble(row[20]), metadata: metadata));
-                metrics.Add(new Metric("pcie.link.width.current", Convert.ToDouble(row[21]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.volatile.device_memory", Convert.ToDouble(row[22]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.volatile.dram", Convert.ToDouble(row[23]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.volatile.sram", Convert.ToDouble(row[24]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.volatile.total", Convert.ToDouble(row[25]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.aggregate.device_memory", Convert.ToDouble(row[26]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.aggregate.dram", Convert.ToDouble(row[27]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.aggregate.sram", Convert.ToDouble(row[28]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.corrected.aggregate.total", Convert.ToDouble(row[29]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.device_memory", Convert.ToDouble(row[30]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.dram", Convert.ToDouble(row[31]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.sram", Convert.ToDouble(row[32]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.total", Convert.ToDouble(row[33]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.device_memory", Convert.ToDouble(row[34]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.dram", Convert.ToDouble(row[35]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.sram", Convert.ToDouble(row[36]), metadata: metadata));
-                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.total", Convert.ToDouble(row[37]), metadata: metadata));
+                metrics.Add(new Metric("utilization.gpu", Convert.ToDouble(SafeGet(row, "utilization.gpu [%]")), unit: "%", metadata: metadata));
+                metrics.Add(new Metric("utilization.memory", Convert.ToDouble(SafeGet(row, "utilization.memory [%]")), unit: "%", metadata: metadata));
+                metrics.Add(new Metric("temperature.gpu", Convert.ToDouble(SafeGet(row, "temperature.gpu")), unit: "celsuis", metadata: metadata));
+                metrics.Add(new Metric("temperature.memory", Convert.ToDouble(SafeGet(row, "temperature.memory")), unit: "celsuis", metadata: metadata));
+                metrics.Add(new Metric("power.draw.average", Convert.ToDouble(SafeGet(row, "power.draw.average [W]")), unit: "W", metadata: metadata));
+                metrics.Add(new Metric("clocks.gr", Convert.ToDouble(SafeGet(row, "clocks.current.graphics [MHz]")), unit: "MHz", metadata: metadata));
+                metrics.Add(new Metric("clocks.sm", Convert.ToDouble(SafeGet(row, "clocks.current.sm [MHz]")), unit: "MHz", metadata: metadata));
+                metrics.Add(new Metric("clocks.video", Convert.ToDouble(SafeGet(row, "clocks.current.video [MHz]")), unit: "MHz", metadata: metadata));
+                metrics.Add(new Metric("clocks.mem", Convert.ToDouble(SafeGet(row, "clocks.current.memory [MHz]")), unit: "MHz", metadata: metadata));
+                metrics.Add(new Metric("memory.total", Convert.ToDouble(SafeGet(row, "memory.total [MiB]")), unit: "MiB", metadata: metadata));
+                metrics.Add(new Metric("memory.free", Convert.ToDouble(SafeGet(row, "memory.free [MiB]")), unit: "MiB", metadata: metadata));
+                metrics.Add(new Metric("memory.used", Convert.ToDouble(SafeGet(row, "memory.used [MiB]")), unit: "MiB", metadata: metadata));
+                metrics.Add(new Metric("power.draw.instant", Convert.ToDouble(SafeGet(row, "power.draw.instant [W]")), unit: "W", metadata: metadata));
+                metrics.Add(new Metric("pcie.link.gen.gpucurrent", Convert.ToDouble(SafeGet(row, "pcie.link.gen.gpucurrent")), metadata: metadata));
+                metrics.Add(new Metric("pcie.link.width.current", Convert.ToDouble(SafeGet(row, "pcie.link.width.current")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.volatile.device_memory", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.volatile.device_memory")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.volatile.dram", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.volatile.dram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.volatile.sram", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.volatile.sram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.volatile.total", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.volatile.total")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.aggregate.device_memory", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.aggregate.device_memory")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.aggregate.dram", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.aggregate.dram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.aggregate.sram", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.aggregate.sram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.corrected.aggregate.total", Convert.ToDouble(SafeGet(row, "ecc.errors.corrected.aggregate.total")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.device_memory", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.volatile.device_memory")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.dram", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.volatile.dram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.sram", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.volatile.sram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.volatile.total", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.volatile.total")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.device_memory", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.aggregate.device_memory")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.dram", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.aggregate.dram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.sram", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.aggregate.sram")), metadata: metadata));
+                metrics.Add(new Metric("ecc.errors.uncorrected.aggregate.total", Convert.ToDouble(SafeGet(row, "ecc.errors.uncorrected.aggregate.total")), metadata: metadata));
             }
-            
+
             return metrics;
+        }
+
+        private static IConvertible SafeGet(DataRow row, string columnName)
+        {
+            return row.Table.Columns.Contains(columnName) ? Convert.ToString(row[columnName]) : null;
         }
     }
 }
