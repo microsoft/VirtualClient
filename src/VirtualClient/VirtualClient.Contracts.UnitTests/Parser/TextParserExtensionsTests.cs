555d50dfb4d2d3dfaffc905cd2a1b33986257887
nchapagain001 <165215502+nchapagain001@users.noreply.github.com>
2024-03-27 14:37:45 -0700

    Adding Extension for Time Translation

--- src/VirtualClient/VirtualClient.Contracts.UnitTests/Parser/TextParserExtensionsTests.cs
+++ src/VirtualClient/VirtualClient.Contracts.UnitTests/Parser/TextParserExtensionsTests.cs
@@ -4,6 +4,7 @@
 namespace VirtualClient.Contracts.Parser
 {
     using NUnit.Framework;
+    using System;
     using VirtualClient.Common;
     using VirtualClient.TestExtensions;
 
@@ -17,13 +18,13 @@ namespace VirtualClient.Contracts.Parser
         [TestCase("1gb", "1073741824")]
         [TestCase("1tb", "1099511627776")]
         [TestCase("1pb", "1125899906842624")]
-        public void TextParserExtensionsTranslateByteUnitAsExpected(string orginalText, string expectedOutput)
+        public void TextParserExtensionsTranslateByteUnitAsExpected(string originalText, string expectedOutput)
         {
-            Assert.IsTrue(string.Equals(TextParsingExtensions.TranslateByteUnit(orginalText), expectedOutput));
+            Assert.IsTrue(string.Equals(TextParsingExtensions.TranslateByteUnit(originalText), expectedOutput));
         }
 
         [Test]
-        [TestCase("100kb",MetricUnit.Kilobytes,"100")]
+        [TestCase("100kb", MetricUnit.Kilobytes, "100")]
         [TestCase("100mb", MetricUnit.Megabytes, "100")]
         [TestCase("100gb", MetricUnit.Gigabytes, "100")]
         [TestCase("100tb", MetricUnit.Terabytes, "100")]
@@ -34,17 +35,62 @@ namespace VirtualClient.Contracts.Parser
         [TestCase("100tb", MetricUnit.Gigabytes, "102400")]
         [TestCase("100pb", MetricUnit.Terabytes, "102400")]
         [TestCase("1tb", MetricUnit.Kilobytes, "1073741824")]
-        public void TextParserExtensionsTranslateStorageByUnitAsExpected(string orginalText, string metricUnit, string expectedOutput)
+        public void TextParserExtensionsTranslateStorageByUnitAsExpected(string originalText, string metricUnit, string expectedOutput)
         {
-            Assert.IsTrue(string.Equals(TextParsingExtensions.TranslateStorageByUnit(orginalText, metricUnit), expectedOutput));
+            Assert.IsTrue(string.Equals(TextParsingExtensions.TranslateStorageByUnit(originalText, metricUnit), expectedOutput));
         }
 
         [Test]
         [TestCase("100k", "100000")]
         [TestCase("100m", "100000000")]
-        public void TextParserExtensionsTranslateNumericUnitAsExpected(string orginalText, string expectedOutput)
+        public void TextParserExtensionsTranslateNumericUnitAsExpected(string originalText, string expectedOutput)
         {
-            Assert.IsTrue(string.Equals(TextParsingExtensions.TranslateNumericUnit(orginalText), expectedOutput));
+            Assert.IsTrue(string.Equals(TextParsingExtensions.TranslateNumericUnit(originalText), expectedOutput));
+        }
+
+        [Test]
+        [TestCase("1h", "3600")]
+        [TestCase("1hrs", "3600")]
+        [TestCase("1hr", "3600")]
+        [TestCase("1hours", "3600")]
+        [TestCase("1hour", "3600")]
+        //
+        [TestCase("1m", "60")]
+        [TestCase("1minutes", "60")]
+        [TestCase("1minute", "60")]
+        //
+        [TestCase("1000ms", "1")]
+        [TestCase("1000milliseconds", "1")]
+        [TestCase("1000millisecond", "1")]
+        //
+        [TestCase("1000000us", "1")]
+        [TestCase("1000000microsecond", "1")]
+        [TestCase("1000000microseconds", "1")]
+        //
+        [TestCase("1000000000ns", "1")]
+        [TestCase("1000000000nanosecond", "1")]
+        [TestCase("1000000000nanoseconds", "1")]
+        //
+        [TestCase("1s", "1")]
+        [TestCase("1second", "1")]
+        [TestCase("2seconds", "2")]
+        public void TextParserExtensionsTranslateTimeUnitToSecondAsExpected(string originalText, string expectedOutput)
+        {
+            string result = TextParsingExtensions.TranslateToSecondUnit(originalText);
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [Test]
+        [TestCase("60s", MetricUnit.Minutes, "1")]
+        [TestCase("24hr", MetricUnit.Minutes, "1440")]
+        [TestCase("24hrs", MetricUnit.Seconds, "86400")]
+        [TestCase("1000ms", MetricUnit.Milliseconds, "1000")]
+        [TestCase("1000ms", MetricUnit.Seconds, "1")]
+        [TestCase("1000000000nanoseconds", MetricUnit.Microseconds, "1000000")]
+        [TestCase("1000000us", MetricUnit.Seconds, "1")]
+        public void TextParserExtensionsTranslateTimeUnitAsExpected(string originalText, string metricUnit, string expectedOutput)
+        {
+            Assert.IsTrue(string.Equals(TextParsingExtensions.TranslateTimeByUnit(originalText, metricUnit), expectedOutput));
         }
     }
 }
