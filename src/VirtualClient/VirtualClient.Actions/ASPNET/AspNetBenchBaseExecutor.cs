3139c79d235f7de5214a73b1f2e9a15eb4b7d18e
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2024-03-28 11:43:26 -0700

    save

---
+++ src/VirtualClient/VirtualClient.Actions/ASPNET/AspNetBenchBaseExecutor.cs
@@ -0,0 +1,282 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO.Abstractions;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.Extensions.DependencyInjection;
+    using VirtualClient.Common;
+    using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+    using VirtualClient.Contracts.Metadata;
+
+    /// <summary>
+    /// The AspNetBench workload executor.
+    /// </summary>
+    public class AspNetBenchBaseExecutor : VirtualClientComponent
+    {
+        private IFileSystem fileSystem;
+        private IPackageManager packageManager;
+        private IStateManager stateManager;
+        private ISystemManagement systemManagement;
+
+        private string dotnetExePath;
+        private string aspnetBenchDirectory;
+        private string aspnetBenchDllPath;
+        private string bombardierFilePath;
+        private string serverArgument;
+        private string clientArgument;
+
+        private Action killServer;
+
+        /// <summary>
+        /// Constructor for <see cref="AspNetBenchExecutor"/>
+        /// </summary>
+        /// <param name="dependencies">Provides required dependencies to the component.</param>
+        /// <param name="parameters">Parameters defined in the profile or supplied on the command line.</param>
+        public AspNetBenchExecutor(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
+             : base(dependencies, parameters)
+        {
+            this.systemManagement = this.Dependencies.GetService<ISystemManagement>();
+            this.packageManager = this.systemManagement.PackageManager;
+            this.stateManager = this.systemManagement.StateManager;
+            this.fileSystem = this.systemManagement.FileSystem;
+        }
+
+        /// <summary>
+        /// The name of the package where the AspNetBench package is downloaded.
+        /// </summary>
+        public string TargetFramework
+        {
+            get
+            {
+                // Lower case to prevent build path issue.
+                return this.Parameters.GetValue<string>(nameof(AspNetBenchExecutor.TargetFramework)).ToLower();
+            }
+        }
+
+        /// <summary>
+        /// The port for ASPNET to run.
+        /// </summary>
+        public string Port
+        {
+            get
+            {
+                // Lower case to prevent build path issue.
+                return this.Parameters.GetValue<string>(nameof(AspNetBenchExecutor.Port), "9876");
+            }
+        }
+
+        /// <summary>
+        /// The name of the package where the bombardier package is downloaded.
+        /// </summary>
+        public string BombardierPackageName
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(AspNetBenchExecutor.BombardierPackageName), "bombardier");
+            }
+        }
+
+        /// <summary>
+        /// The name of the package where the DotNetSDK package is downloaded.
+        /// </summary>
+        public string DotNetSdkPackageName
+        {
+            get
+            {
+                return this.Parameters.GetValue<string>(nameof(AspNetBenchExecutor.DotNetSdkPackageName), "dotnetsdk");
+            }
+        }
+
+        /// <summary>
+        /// Returns true/false whether the component is supported on the current
+        /// OS platform and CPU architecture.
+        /// </summary>
+        protected override bool IsSupported()
+        {
+            bool isSupported = base.IsSupported()
+                && (this.Platform == PlatformID.Win32NT || this.Platform == PlatformID.Unix)
+                && (this.CpuArchitecture == Architecture.X64 || this.CpuArchitecture == Architecture.Arm64);
+
+            if (!isSupported)
+            {
+                this.Logger.LogNotSupported("AspNetBench", this.Platform, this.CpuArchitecture, EventContext.Persisted());
+            }
+
+            return isSupported;
+        }
+
+        /// <summary>
+        /// Executes the AspNetBench workload.
+        /// </summary>
+        protected override async Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            Task serverTask = this.StartAspNetServerAsync(cancellationToken);
+            await this.RunBombardierAsync(telemetryContext, cancellationToken)
+                .ConfigureAwait(false);
+
+            this.killServer.Invoke();
+        }
+
+        /// <summary>
+        /// Initializes the environment for execution of the AspNetBench workload.
+        /// </summary>
+        protected override async Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            // This workload needs three packages: aspnetbenchmarks, dotnetsdk, bombardier
+            DependencyPath workloadPackage = await this.packageManager.GetPackageAsync(this.PackageName, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            if (workloadPackage == null)
+            {
+                throw new DependencyException(
+                    $"The expected package '{this.PackageName}' does not exist on the system or is not registered.",
+                    ErrorReason.WorkloadDependencyMissing);
+            }
+
+            // the directory we are looking for is at the src/Benchmarks
+            this.aspnetBenchDirectory = this.Combine(workloadPackage.Path, "src", "Benchmarks");
+
+            DependencyPath bombardierPackage = await this.packageManager.GetPlatformSpecificPackageAsync(this.BombardierPackageName, this.Platform, this.CpuArchitecture, cancellationToken)
+                .ConfigureAwait(false);
+
+            this.bombardierFilePath = this.Combine(bombardierPackage.Path, this.Platform == PlatformID.Unix ? "bombardier" : "bombardier.exe");
+
+            await this.systemManagement.MakeFileExecutableAsync(this.bombardierFilePath, this.Platform, cancellationToken)
+                .ConfigureAwait(false);
+
+            DependencyPath dotnetSdkPackage = await this.packageManager.GetPackageAsync(this.DotNetSdkPackageName, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            if (dotnetSdkPackage == null)
+            {
+                throw new DependencyException(
+                    $"The expected DotNet SDK package does not exist on the system or is not registered.",
+                    ErrorReason.WorkloadDependencyMissing);
+            }
+
+            this.dotnetExePath = this.Combine(dotnetSdkPackage.Path, this.Platform == PlatformID.Unix ? "dotnet" : "dotnet.exe");
+
+            // ~/vc/packages/dotnet/dotnet build -c Release -p:BenchmarksTargetFramework=net8.0
+            // Build the aspnetbenchmark project
+            string buildArgument = $"build -c Release -p:BenchmarksTargetFramework={this.TargetFramework}";
+            await this.ExecuteCommandAsync(this.dotnetExePath, buildArgument, this.aspnetBenchDirectory, cancellationToken)
+                .ConfigureAwait(false);
+
+            // "C:\Users\vcvmadmin\Benchmarks\src\Benchmarks\bin\Release\net8.0\Benchmarks.dll"
+            this.aspnetBenchDllPath = this.Combine(
+                this.aspnetBenchDirectory, 
+                "bin", 
+                "Release", 
+                this.TargetFramework, 
+                "Benchmarks.dll");
+        }
+
+        private void CaptureMetrics(IProcessProxy process, EventContext telemetryContext)
+        {
+            try
+            {
+                this.MetadataContract.AddForScenario(
+                    "AspNetBench",
+                    $"{this.clientArgument},{this.serverArgument}",
+                    toolVersion: null);
+
+                this.MetadataContract.Apply(telemetryContext);
+
+                BombardierMetricsParser parser = new BombardierMetricsParser(process.StandardOutput.ToString());
+
+                this.Logger.LogMetrics(
+                    toolName: "AspNetBench",
+                    scenarioName: $"ASP.NET_{this.TargetFramework}_Performance",
+                    process.StartTime,
+                    process.ExitTime,
+                    parser.Parse(),
+                    metricCategorization: "json",
+                    scenarioArguments: $"Client: {this.clientArgument} | Server: {this.serverArgument}",
+                    this.Tags,
+                    telemetryContext);
+            }
+            catch (Exception exc)
+            {
+                throw new WorkloadResultsException($"Failed to parse bombardier output.", exc, ErrorReason.InvalidResults);
+            }
+        }
+
+        private Task StartAspNetServerAsync(CancellationToken cancellationToken)
+        {
+            // Example:
+            // dotnet <path_to_binary>\Benchmarks.dll --nonInteractive true --scenarios json --urls http://localhost:5000 --server Kestrel --kestrelTransport Sockets --protocol http
+            // --header "Accept: application/json,text/html;q=0.9,application/xhtml+xml;q=0.9,application/xml;q=0.8,*/*;q=0.7" --header "Connection: keep-alive" 
+
+            string options = $"--nonInteractive true --scenarios json --urls http://localhost:{this.Port} --server Kestrel --kestrelTransport Sockets --protocol http";
+            string headers = @"--header ""Accept: application/json,text/html;q=0.9,application/xhtml+xml;q=0.9,application/xml;q=0.8,*/*;q=0.7"" --header ""Connection: keep-alive""";
+            this.serverArgument = $"{this.aspnetBenchDllPath} {options} {headers}";
+
+            return this.ExecuteCommandAsync(this.dotnetExePath, this.serverArgument, this.aspnetBenchDirectory, cancellationToken, isServer: true);
+        }
+
+        private async Task RunBombardierAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            using (BackgroundOperations profiling = BackgroundOperations.BeginProfiling(this, cancellationToken))
+            {
+                // https://pkg.go.dev/github.com/codesenberg/bombardier
+                // ./bombardier --duration 15s --connections 256 --timeout 10s --fasthttp --insecure -l http://localhost:5000/json --print r --format json
+                this.clientArgument = $"--duration 15s --connections 256 --timeout 10s --fasthttp --insecure -l http://localhost:{this.Port}/json --print r --format json";
+
+                using (IProcessProxy process = await this.ExecuteCommandAsync(this.bombardierFilePath, this.clientArgument, this.aspnetBenchDirectory, telemetryContext, cancellationToken, runElevated: true)
+                    .ConfigureAwait(false))
+                {
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        await this.LogProcessDetailsAsync(process, telemetryContext, "AspNetBench", logToFile: true);
+
+                        process.ThrowIfWorkloadFailed();
+                        this.CaptureMetrics(process, telemetryContext);
+                    }
+                }
+            }
+        }
+
+        private async Task ExecuteCommandAsync(string pathToExe, string commandLineArguments, string workingDirectory, CancellationToken cancellationToken, bool isServer = false)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                this.Logger.LogTraceMessage($"Executing process '{pathToExe}' '{commandLineArguments}' at directory '{workingDirectory}'.");
+
+                EventContext telemetryContext = EventContext.Persisted()
+                    .AddContext("command", pathToExe)
+                    .AddContext("commandArguments", commandLineArguments);
+
+                using (IProcessProxy process = this.systemManagement.ProcessManager.CreateElevatedProcess(this.Platform, pathToExe, commandLineArguments, workingDirectory))
+                {
+                    if (isServer)
+                    {
+                        this.killServer = () => process.SafeKill();
+                    }
+
+                    this.CleanupTasks.Add(() => process.SafeKill());
+                    await process.StartAndWaitAsync(cancellationToken).ConfigureAwait(false);
+                        
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        await this.LogProcessDetailsAsync(process, telemetryContext);
+
+                        if (!isServer)
+                        {
+                            // We will kill the server at the end, exit code is -1, and we don't want it to log as failure.
+                            process.ThrowIfWorkloadFailed();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Core/VirtualClientMultiRoleComponent.cs
@@ -0,0 +1,122 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Logging.Abstractions;
+    using Newtonsoft.Json.Linq;
+    using Polly;
+    using VirtualClient;
+    using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts.Metadata;
+
+    /// <summary>
+    /// The base class for all Virtual Client profile actions and monitors.
+    /// </summary>
+    public abstract class VirtualClientMultiRoleComponent : VirtualClientComponent
+    {
+        private ISystemManagement systemManagement;
+        private IApiClientManager apiClientManager;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dependencies"></param>
+        /// <param name="parameters"></param>
+        protected VirtualClientMultiRoleComponent(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters = null)
+            : base(dependencies, parameters)
+        {
+            this.systemManagement = dependencies.GetService<ISystemManagement>();
+            this.apiClientManager = dependencies.GetService<IApiClientManager>();
+        }
+
+        /// <summary>
+        /// General retry policy when used in talking with other clients.
+        /// </summary>
+        protected IAsyncPolicy RetryPolicy { get; set; } =
+            Policy.Handle<Exception>(exc => !(exc is OperationCanceledException))
+                .WaitAndRetryAsync(3, (retries) => TimeSpan.FromSeconds(retries));
+
+        /// <summary>
+        /// The timespan at which the client will poll the server for responses before
+        /// timing out.
+        /// </summary>
+        protected TimeSpan PollingTimeout { get; set; } = TimeSpan.FromMinutes(30);
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected Task WaitForRoleAsync(string role, CancellationToken cancellationToken)
+        {
+            IPAddress ipAddress;
+            List<Task> clientWorkloadTasks = new List<Task>();
+
+            if (this.IsMultiRoleLayout())
+            {
+                IEnumerable<ClientInstance> targetServers = this.GetLayoutClientInstances(role);
+                foreach (ClientInstance server in targetServers)
+                {
+                    // Reliability/Recovery:
+                    // The pattern here is to allow for any steps within the workflow to fail and to simply start the entire workflow
+                    // over again.
+                    clientWorkloadTasks.Add(this.RetryPolicy.ExecuteAsync(async () =>
+                    {
+                        if (!cancellationToken.IsCancellationRequested)
+                        {
+                            IApiClient serverApiClient = this.ApiClientManager.GetOrCreateApiClient(server.Name, server);
+
+                            // 1) Confirm server is online.
+                            // ===========================================================================
+                            this.Logger.LogTraceMessage("Synchronization: Poll server API for heartbeat...");
+                            await serverApiClient.PollForHeartbeatAsync(this.PollingTimeout, cancellationToken);
+
+                            // 2) Confirm the server-side application (e.g. web server) is online.
+                            // ===========================================================================
+                            this.Logger.LogTraceMessage("Synchronization: Poll server for online signal...");
+                            await serverApiClient.PollForServerOnlineAsync(TimeSpan.FromSeconds(30), cancellationToken);
+
+                            this.Logger.LogTraceMessage("Synchronization: Server online signal confirmed...");
+                            this.Logger.LogTraceMessage("Synchronization: Start client workload...");
+
+                            // 3) Get Parameters required.
+                            ServerState serverState = await this.GetServerStateAsync(serverApiClient, cancellationToken);
+
+                            // 4) Execute the client workload.
+                            // ===========================================================================
+                            ipAddress = IPAddress.Parse(server.IPAddress);
+                            await this.ExecuteWorkloadsAsync(ipAddress, serverState, telemetryContext, cancellationToken);
+                        }
+                    }));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected Task TerminateRoleAsync(string role, CancellationToken cancellationToken)
+        {
+
+        }
+
+
+    }
+}
