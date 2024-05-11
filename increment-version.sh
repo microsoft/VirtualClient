363b94b3a4d83f5399c281ff88e34e9e9c37bf46
saibulusu <saibulusu@microsoft.com>
2024-05-10 22:16:21 -0700

    Using a single version file, and using virtualclient-ms for the updates.

--- increment-version.sh
+++ increment-version.sh
@@ -8,14 +8,14 @@ calculate_new_version(){
     echo "$major.$minor.$patch"
 }
 
-current_version=$(yq '.variables.VcVersion' .pipelines/azure-pipelines.yml)
+current_version=$(<VERSION)
 echo "current version: $current_version"
 
 git config --global advice.detachedHead false
 
 git stash
 git checkout HEAD~1
-previous_version=$(yq '.variables.VcVersion' .pipelines/azure-pipelines.yml)
+previous_version=$(<VERSION)
 echo "previous version: $previous_version"
 
 git checkout main
@@ -25,15 +25,11 @@ if [ "$previous_version" == "$current_version" ]; then
     new_version=$(calculate_new_version $current_version)
     echo "new version: $new_version"
 
-    key="VcVersion"
-    new_value=$new_version
-    yaml_file=".pipelines/azure-pipelines.yml"
+    echo "$new_version" > VERSION
 
-    sed -r "s/^(\\s*${key}\\s*:\\s*).*/\\1${new_value}/" -i "$yaml_file"
-
-    git config user.email "sai.bulusu@gmail.com"
-    git config user.name "saibulusu"
-    git add -f .pipelines/azure-pipelines.yml
+    git config user.email "virtualclient@microsoft.com"
+    git config user.name "virtualclient-ms"
+    git add -f VERSION
     git commit -m "Updating to $new_version"
     git push
 else
