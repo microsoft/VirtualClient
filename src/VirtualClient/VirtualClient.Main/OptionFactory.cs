df1413e8712767dbb21fa888a1b2203aed6c1a91
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2024-05-13 09:02:08 -0700

    Hotfix to add eventhubconnectionstring argumnet back (#313)
    * Fix issue in VC versioning
    
    * Hotfix to add eventhubconnectionstring argumnet back

--- src/VirtualClient/VirtualClient.Main/OptionFactory.cs
+++ src/VirtualClient/VirtualClient.Main/OptionFactory.cs
@@ -269,7 +269,7 @@ namespace VirtualClient
         public static Option CreateEventHubAuthenticationContextOption(bool required = false, object defaultValue = null)
         {
             Option<EventHubAuthenticationContext> option = new Option<EventHubAuthenticationContext>(
-                new string[] { "--event-hub", "--eventHub", "--eventhub", "--eh" },
+                new string[] { "--event-hub", "--eventHub", "--eventhub", "--eh", "--eventbHubConnectionString" },
                 new ParseArgument<EventHubAuthenticationContext>(result => OptionFactory.ParseEventHubAuthenticationContext(result)))
             {
                 Name = "EventHubAuthenticationContext",
