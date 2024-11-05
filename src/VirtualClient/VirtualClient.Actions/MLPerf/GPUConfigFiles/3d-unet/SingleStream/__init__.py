ace8faa3ca0be87703d10b47bfd50f98aef1e087
saibulusu <saibulusu@microsoft.com>
2024-11-04 22:31:12 -0800

    Changing expected latency for 3d-unet single stream.

--- src/VirtualClient/VirtualClient.Actions/MLPerf/GPUConfigFiles/3d-unet/SingleStream/__init__.py
+++ src/VirtualClient/VirtualClient.Actions/MLPerf/GPUConfigFiles/3d-unet/SingleStream/__init__.py
@@ -381,7 +381,7 @@ class A100_SXM4_40GBx8(A100_SXM4_40GBx1):
     gpu_batch_size = {'3d-unet': 8}
     start_from_device = True
     end_on_device = True
-    single_stream_expected_latency_ns = 657000000
+    single_stream_expected_latency_ns = 520000000
 
 
 @ConfigRegistry.register(HarnessType.Custom, AccuracyTarget.k_99, PowerSetting.MaxP)
