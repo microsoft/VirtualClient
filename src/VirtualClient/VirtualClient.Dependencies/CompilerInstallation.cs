0b481fc7152f6b0f5fd9d9c3a1bcb3158bab38e0
saibulusu <saibulusu@microsoft.com>
2025-01-22 10:57:36 -0800

    Adding make for RHEL/Cent OS.

--- src/VirtualClient/VirtualClient.Dependencies/CompilerInstallation.cs
+++ src/VirtualClient/VirtualClient.Dependencies/CompilerInstallation.cs
@@ -239,7 +239,7 @@ namespace VirtualClient.Dependencies
                         await this.ExecuteCommandAsync("dnf", "install binutils -y", Environment.CurrentDirectory, telemetryContext, cancellationToken);
                         await this.ExecuteCommandAsync("dnf", "install glibc-headers glibc-devel -y", Environment.CurrentDirectory, telemetryContext, cancellationToken);
                         await this.ExecuteCommandAsync("dnf", "install git -y", Environment.CurrentDirectory, telemetryContext, cancellationToken);
-                        await this.ExecuteCommandAsync("dnf", "install gcc -y", Environment.CurrentDirectory, telemetryContext, cancellationToken);
+                        await this.ExecuteCommandAsync("dnf", "install make gcc -y", Environment.CurrentDirectory, telemetryContext, cancellationToken);
                     }
                     else
                     {
