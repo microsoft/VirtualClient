9feec02372c271829f4c568f16438fa2e08b0dee
Yang Pan <31965446+yangpanMS@users.noreply.github.com>
2025-02-19 22:20:18 -0800

    fix unit test

--- src/VirtualClient/VirtualClient.UnitTests/OptionFactoryTests.cs
+++ src/VirtualClient/VirtualClient.UnitTests/OptionFactoryTests.cs
@@ -508,10 +508,9 @@ namespace VirtualClient
         }
 
         [Test]
-        [Platform(Include = "Win"), TestCase("C:\\Any\\Directory\\Path")]
-        [Platform(Include = "Unix"), TestCase("/home/any/directory/path")]
-        public void LogDirectoryOptionSupportsFullPaths(string path)
+        public void LogDirectoryOptionSupportsFullPaths()
         {
+            string path = OperatingSystem.IsWindows() ? "C:\\Any\\Directory\\Path" : "/home/any/directory/path";
             Option option = OptionFactory.CreateLogDirectoryOption();
             ParseResult result = option.Parse($"--log-dir={path}");
 
@@ -707,10 +706,9 @@ namespace VirtualClient
         }
 
         [Test]
-        [Platform(Include = "Win"), TestCase("C:\\Any\\Directory\\Path")]
-        [Platform(Include = "Unix"), TestCase("/home/any/directory/path")]
-        public void PackageDirectoryOptionSupportsFullPaths(string path)
+        public void PackageDirectoryOptionSupportsFullPaths()
         {
+            string path = OperatingSystem.IsWindows() ? "C:\\Any\\Directory\\Path" : "/home/any/directory/path";
             Option option = OptionFactory.CreatePackageDirectoryOption();
             ParseResult result = option.Parse($"--package-dir={path}");
 
@@ -1060,10 +1058,9 @@ namespace VirtualClient
         }
 
         [Test]
-        [Platform(Include = "Win"), TestCase("C:\\Any\\Directory\\Path")]
-        [Platform(Include = "Unix"), TestCase("/home/any/directory/path")]
-        public void StateDirectoryOptionSupportsFullPaths(string path)
+        public void StateDirectoryOptionSupportsFullPaths()
         {
+            string path = OperatingSystem.IsWindows() ? "C:\\Any\\Directory\\Path" : "/home/any/directory/path";
             Option option = OptionFactory.CreateStateDirectoryOption();
             ParseResult result = option.Parse($"--state-dir={path}");
 
