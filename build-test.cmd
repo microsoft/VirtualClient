4834a95a3f94d185b5b2b00de5ce283e3b43eb49
brdeyo <brdeyo@microsoft.com>
2025-03-15 14:25:56 -0700

    Remove AssemblyVersion definition accidentally left in Module.props. Refactor build scripts for consistency with other repo usages.

--- build-test.cmd
+++ build-test.cmd
@@ -1,33 +1,71 @@
 @echo Off
 
-set ExitCode=0
+set EXIT_CODE=0
+set BUILD_CONFIGURATION=Release
+set SCRIPT_DIR=%~dp0
+set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
 
 if /i "%~1" == "/?" Goto :Usage
 if /i "%~1" == "-?" Goto :Usage
 if /i "%~1" == "--help" Goto :Usage
+
+for %%a in (%*) do (
+
+    rem Build Configurations:
+    rem 1) Release (Default)
+    rem 2) Debug
+    rem
+    rem Pass in the --debug flag to use 'Debug' build configuration
+    if /i "%%a" == "--debug" set BUILD_CONFIGURATION=Debug
+)
+
+echo ********************************************************************
+echo Build Version   : %VCBuildVersion%
+echo Repo Root       : %SCRIPT_DIR%
+echo Configuration   : %BUILD_CONFIGURATION%
+echo Tests Directory : %SCRIPT_DIR%\out\bin\%BUILD_CONFIGURATION%
+echo ********************************************************************
 
 echo:
 echo [Running Tests]
 echo --------------------------------------------------
 
 for /f "tokens=*" %%f in ('dir /B /S %~dp0src\*Tests.csproj') do (
-    call dotnet test -c Release %%f --no-restore --no-build --filter "(Category=Unit|Category=Functional)" --logger "console;verbosity=normal" && echo: || Goto :Error
+    call dotnet test -c %BUILD_CONFIGURATION% %%f --no-restore --no-build --filter "(Category=Unit|Category=Functional)" --logger "console;verbosity=normal" && echo: || Goto :Error
 )
 
 Goto :End
 
+
 :Usage
+echo:
+echo Discovers and runs tests (unit + functional) defined in the build output/artifacts.
+echo:
+echo Options:
+echo ---------------------
+echo --debug  - Flag requests tests for build configuration 'Debug' vs. the default 'Release'.
 echo:
 echo Usage:
 echo ---------------------
-echo %~0
+echo build-test.cmd [--debug]
+echo:
+echo Examples:
+echo ---------------------
+echo %SCRIPT_DIR%^> build-test.cmd
+echo:
+echo %SCRIPT_DIR%^> build-test.cmd --debug
 Goto :Finish
 
+
 :Error
-set ExitCode=%ERRORLEVEL%
+set EXIT_CODE=%ERRORLEVEL%
+
 
 :End
-echo Test Stage Exit Code: %ExitCode%
+set BUILD_CONFIGURATION=
+set SCRIPT_DIR=
+echo Test Stage Exit Code: %EXIT_CODE%
+
 
 :Finish
-exit /B %ExitCode%
+exit /B %EXIT_CODE%
--- build.sh
+++ build.sh
@@ -1,46 +1,136 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
-    VCBuildVersion="$1"
+EXIT_CODE=0
+BUILD_CONFIGURATION="Release"
+BUILD_FLAGS="-p:PublishTrimmed=true"
+BUILD_VERSION=""
+SCRIPT_DIR="$(dirname $(readlink -f "${BASH_SOURCE}"))"
+
+Usage() {
+    echo ""
+    echo "Builds the source code in the repo. "
+    echo ""
+    echo "Options:"
+    echo "---------------------"
+    echo "--debug  - Flag requests build configuration to be 'Debug' vs. the default 'Release'."
+    echo ""
+    echo "Usage:"
+    echo "---------------------"
+    echo "build.sh [--debug]"
+    echo ""
+    echo "Examples"
+    echo "---------------------"
+    echo "user@system:~/repo$ chmod +x *.sh"
+    echo "user@system:~/repo$ ./build.sh"
+    echo ""
+    echo "user@system:~/repo$ chmod +x *.sh"
+    echo "user@system:~/repo$ ./build.sh --debug"
+    echo ""
+    echo "user@system:~/repo$ export VCBuildVersion=\"1.16.25\""
+    echo "user@system:~/repo$ ./build.sh --debug"
+    echo ""
+    Finish
+}
+
+Error() {
+    EXIT_CODE=1
+    End
+}
+
+End() {
+    echo ""
+    echo "Build Stage Exit Code: $EXIT_CODE"
+    echo ""
+    Finish
+}
+
+Finish() {
+    exit $EXIT_CODE
+}
+
+if [ "${1,,}" == "/?" ] || [ "${1,,}" == "-?" ] || [ "${1,,}" == "--help" ]; then
+    Usage
 fi
 
-if [ -z "$VCBuildVersion" ]; then
-    VCBuildVersion=$(cat VERSION)
+if [ "${1,,}" == "--debug" ]; then
+    # Build Configurations:
+    # 1) Release (Default)
+    # 2) Debug
+    #
+    # Pass in the --debug flag to use 'Debug' build configuration
+    BUILD_CONFIGURATION="Debug"
 fi
 
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --build-all)
-            BUILD_ALL=true
-            ;;
-        *)
-            ;;
-    esac
-    shift
-done
+# The default build version is defined in the repo VERSION file.
+BUILD_VERSION=$(cat "$SCRIPT_DIR/VERSION") | 's/^[[:space:]]*//;s/[[:space:]]*$//'
 
-PUBLISH_FLAGS="--self-contained -p:InvariantGlobalization=true"
-ARCH=$(uname -m)
-
-if [ "$ARCH" == "x86_64" ]; then
-    ARCH="linux-x64"
-elif [ "$ARCH" == "aarch64" ]; then
-    ARCH="linux-arm64"
-else
-    echo "Unsupported architecture: $ARCH"
+# The default build version can be overridden by the 'VCBuildVersion' 
+# environment variable
+if [[ -v "VCBuildVersion" && -n "$VCBuildVersion" ]]; then
+    BUILD_VERSION=$VCBuildVersion
 fi
 
-echo "Building VirtualClient solution."
-dotnet build src/VirtualClient/VirtualClient.sln -c Release -p:VCBuildVersion=$VCBuildVersion
+echo ""
+echo "**********************************************************************"
+echo "Build Version : $BUILD_VERSION"
+echo "Repo Root     : $SCRIPT_DIR"
+echo "Configuration : $BUILD_CONFIGURATION"
+echo "Flags         : $BUILD_FLAGS"
+echo "**********************************************************************"
 
+echo ""
+echo "[Build Solution]"
+echo "----------------------------------------------------------------------"
+dotnet build "$SCRIPT_DIR/src/VirtualClient/VirtualClient.sln" -c Release $BUILD_CONFIGURATION \
+-p:AssemblyVersion=%BUILD_VERSION%
 
-if [ "$BUILD_ALL" = true ]; then
-    echo "Publishing VirtualClient for all platforms."
-    dotnet publish src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj -r linux-x64 -c Release $PUBLISH_FLAGS
-    dotnet publish src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj -r linux-arm64 -c Release $PUBLISH_FLAGS
-    dotnet publish src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj -r win-x64 -c Release $PUBLISH_FLAGS
-    dotnet publish src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj -r win-arm64 -c Release $PUBLISH_FLAGS
-else
-    echo "Publishing VirtualClient for architecture: $ARCH"
-    dotnet publish src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj -r $ARCH -c Release $PUBLISH_FLAGS
+result=$?
+if [ $result -ne 0 ]; then
+    Error
 fi
+
+echo ""
+echo "[Build Virtual Client: linux-x64]"
+echo "----------------------------------------------------------------------"
+dotnet publish "$SCRIPT_DIR/src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj" -r linux-x64 -c $BUILD_CONFIGURATION --self-contained \
+-p:AssemblyVersion=%BUILD_VERSION% $BUILD_FLAGS
+
+result=$?
+if [ $result -ne 0 ]; then
+    Error
+fi
+
+echo ""
+echo "Build Virtual Client: linux-arm64]"
+echo "----------------------------------------------------------------------"
+dotnet publish "$SCRIPT_DIR/src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj" -r linux-arm64 -c $BUILD_CONFIGURATION --self-contained \
+-p:AssemblyVersion=%BUILD_VERSION% $BUILD_FLAGS
+
+result=$?
+if [ $result -ne 0 ]; then
+    Error
+fi
+
+echo ""
+echo "Build Virtual Client: win-x64]"
+echo "----------------------------------------------------------------------"
+dotnet publish "$SCRIPT_DIR/src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj" -r win-x64 -c $BUILD_CONFIGURATION --self-contained \
+-p:AssemblyVersion=%BUILD_VERSION% $BUILD_FLAGS
+
+result=$?
+if [ $result -ne 0 ]; then
+    Error
+fi
+
+echo ""
+echo "[Build Virtual Client: win-arm64]"
+echo "----------------------------------------------------------------------"
+dotnet publish "$SCRIPT_DIR/src/VirtualClient/VirtualClient.Main/VirtualClient.Main.csproj" -r win-arm64 -c $BUILD_CONFIGURATION --self-contained \
+-p:AssemblyVersion=%BUILD_VERSION% $BUILD_FLAGS
+
+result=$?
+if [ $result -ne 0 ]; then
+    Error
+fi
+
+End
