3e202bff86225cc6685ac59b07d9ea3e066a70b7
Bryan DeYoung <35380894+brdeyo@users.noreply.github.com>
2025-04-04 08:47:09 -0700

    Bug fix. Change Serilog file logger configuration to one that correctly rolls log files based on size. (#481)

--- src/VirtualClient/VirtualClient.Contracts.UnitTests/ComponentTypeCacheTests.cs
+++ src/VirtualClient/VirtualClient.Contracts.UnitTests/ComponentTypeCacheTests.cs
@@ -13,6 +13,7 @@ namespace VirtualClient.Contracts
     internal class ComponentTypeCacheTests
     {
         [Test]
+        [Order(1)]
         public void ComponentTypeCacheLoadsExpectedTypesFromComponentAssemblies()
         {
             lock (ComponentTypeCache.LockObject)
@@ -28,6 +29,29 @@ namespace VirtualClient.Contracts
 
                     Assert.IsNotEmpty(ComponentTypeCache.Instance);
                     Assert.IsTrue(ComponentTypeCache.Instance.All(t => expectedTypes.Any(et => t.Type.IsAssignableTo(et))));
+                }
+                finally
+                {
+                    ComponentTypeCache.Instance.Clear();
+                }
+            }
+        }
+
+        [Test]
+        [Order(2)]
+        public void ComponentTypeCacheHandlesAttemptsToLoadAssembliesAlreadyLoaded()
+        {
+            lock (ComponentTypeCache.LockObject)
+            {
+                try
+                {
+                    List<Type> expectedTypes = new List<Type>
+                    {
+                        typeof(VirtualClientComponent)
+                    };
+
+                    ComponentTypeCache.Instance.LoadComponentTypes(MockFixture.TestAssemblyDirectory);
+                    Assert.DoesNotThrow(() => ComponentTypeCache.Instance.LoadComponentTypes(MockFixture.TestAssemblyDirectory));
                 }
                 finally
                 {
