54f85469450f9d25458f1c6cef75ceae2a331b79
saibulusu <saibulusu@microsoft.com>
2025-05-21 14:39:03 -0700

    Fixing certificate manager unit tests.

--- src/VirtualClient/VirtualClient.Core.UnitTests/Identity/CertificateManagerTest.cs
+++ src/VirtualClient/VirtualClient.Core.UnitTests/Identity/CertificateManagerTest.cs
@@ -35,13 +35,13 @@ namespace VirtualClient.Identity
         [Test]
         [TestCase("AME")]
         [TestCase("GBL")]
-        [TestCase("AME Infra CA 06")]
+        [TestCase("AME Infra CA 01")]
         [TestCase("DC=AME")]
         [TestCase("DC=GBL")]
         [TestCase("CN=AME")]
-        [TestCase("CN=AME Infra CA 06")]
-        [TestCase("CN=AME Infra CA 06, DC=AME, DC=GBL")]
-        [TestCase("CN=AME Infra CA 06,DC=AME,DC=GBL")]
+        [TestCase("CN=AME Infra CA 01")]
+        [TestCase("CN=AME Infra CA 01, DC=AME, DC=GBL")]
+        [TestCase("CN=AME Infra CA 01,DC=AME,DC=GBL")]
         public void CertificateManagerSearchesSupportsARangeOfFormatsForIssuersOnCertificates(string issuer)
         {
             X509Certificate2 certificate = this.mockFixture.Create<X509Certificate2>();
@@ -50,15 +50,15 @@ namespace VirtualClient.Identity
 
         [Test]
         [TestCase("ABC")]
-        [TestCase("AME Infra CA 01")]
+        [TestCase("AME Infra CA 06")]
         [TestCase("DC=ABC")]
         [TestCase("DC=GBB")]
-        [TestCase("DC=AME Infra CA 01")]
+        [TestCase("DC=AME Infra CA 06")]
         [TestCase("CN=ABC")]
-        [TestCase("CN=AME Infra CA 01")]
+        [TestCase("CN=AME Infra CA 06")]
         [TestCase("CN=ABC Infra CA 01, DC=AME, DC=GBL")]
-        [TestCase("CN=AME Infra CA 01, DC=ABC, DC=GBL")]
-        [TestCase("CN=AME Infra CA 01, DC=AME, DC=GBB")]
+        [TestCase("CN=AME Infra CA 06, DC=ABC, DC=GBL")]
+        [TestCase("CN=AME Infra CA 06, DC=AME, DC=GBB")]
         public void CertificateManagerDoesNotMismatchIssuersOnCertificates(string issuer)
         {
             X509Certificate2 certificate = this.mockFixture.Create<X509Certificate2>();
@@ -147,13 +147,13 @@ namespace VirtualClient.Identity
         [Test]
         [TestCase("AME", "virtualclient.test.corp.azure.com")]
         [TestCase("GBL", "virtualclient.test.corp.azure.com")]
-        [TestCase("AME Infra CA 06", "virtualclient")]
+        [TestCase("AME Infra CA 01", "virtualclient")]
         [TestCase("DC=AME", "corp.azure.com")]
         [TestCase("DC=GBL", "azure.com")]
         [TestCase("CN=AME", "virtualclient.test.corp.azure.com")]
-        [TestCase("CN=AME Infra CA 06", "CN=virtualclient.test.corp.azure.com")]
-        [TestCase("CN=AME Infra CA 06, DC=AME, DC=GBL", "CN=virtualclient.test.corp.azure.com")]
-        [TestCase("CN=AME Infra CA 06,DC=AME,DC=GBL", "CN=virtualclient.test.corp.azure.com")]
+        [TestCase("CN=AME Infra CA 01", "CN=virtualclient.test.corp.azure.com")]
+        [TestCase("CN=AME Infra CA 01, DC=AME, DC=GBL", "CN=virtualclient.test.corp.azure.com")]
+        [TestCase("CN=AME Infra CA 01,DC=AME,DC=GBL", "CN=virtualclient.test.corp.azure.com")]
         public async Task CertificateManagerHandlesDifferentIssuerAndSubjectNameFormats(string issuer, string subjectName)
         {
             this.mockFixture.Setup(PlatformID.Unix);
