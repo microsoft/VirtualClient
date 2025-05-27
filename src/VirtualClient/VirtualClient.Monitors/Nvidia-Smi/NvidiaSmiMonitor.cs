7c401be57ad5aa3f70d4035b39d4b2cbf8f2ecb6
Deepanshu Vaid <devaid@microsoft.com>
2025-05-27 17:54:53 +0530

    adding iteration back

--- src/VirtualClient/VirtualClient.Monitors/Nvidia-Smi/NvidiaSmiMonitor.cs
+++ src/VirtualClient/VirtualClient.Monitors/Nvidia-Smi/NvidiaSmiMonitor.cs
@@ -53,12 +53,12 @@ namespace VirtualClient.Monitors
                             {
                                 try
                                 {
+                                    iterations++;
                                     if (this.IsIterationComplete(iterations))
                                     {
                                         break;
                                     }
 
-                                    iterations++;
                                     await this.QueryC2CAsync(telemetryContext, cancellationToken);
                                     await this.QueryGpuAsync(telemetryContext, cancellationToken);
                                     await this.WaitAsync(this.MonitorFrequency, cancellationToken);
