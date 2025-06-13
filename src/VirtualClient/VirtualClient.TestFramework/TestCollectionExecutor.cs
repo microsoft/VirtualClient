78d23cb9bb804c12c5b1a140d02b1fe2609bfa7d
Christopher Hillbrand <cjhillbrand@gmail.com>
2025-06-13 11:34:47 -0700

    Filter Child Components dictated by the User-Provided Scenario Options. (#528)
    * Filter child components logic.
    
    * Unit tests and up version.
    
    * Additional Unit tests exercsing a non-duplicated scenario
    
    * One last test to exercise exclusion scenario.
    
    ---------
    
    Co-authored-by: CJ Hillbrand <cjhillbrand@microsoft.com>

---
+++ src/VirtualClient/VirtualClient.TestFramework/TestCollectionExecutor.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// A test/fake executor that can be used in unit and functional testing scenarios.
+    /// </summary>
+    public class TestCollectionExecutor : VirtualClientComponentCollection
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestExecutor"/> class.
+        /// </summary>
+        public TestCollectionExecutor(MockFixture fixture)
+            : base(fixture?.Dependencies, fixture?.Parameters)
+        {
+            this.LogToFile = true;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestExecutor"/> class.
+        /// </summary>
+        public TestCollectionExecutor(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters)
+            : base(dependencies, parameters)
+        {
+            this.LogToFile = true;
+        }
+
+        /// <summary>
+        /// Delegate/anonymous function can be used to inject behaviors into the executor when
+        /// the <see cref="ExecuteAsync(EventContext, CancellationToken)"/> method is called.
+        /// </summary>
+        public Action<EventContext, CancellationToken> OnExecute { get; set; }
+
+        protected override Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            this.OnExecute?.Invoke(telemetryContext, cancellationToken);
+            return Task.CompletedTask;
+        }
+    }
+}
