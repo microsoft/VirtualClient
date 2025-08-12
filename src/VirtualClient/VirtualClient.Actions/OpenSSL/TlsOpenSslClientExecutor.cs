72cc6f8062d5fd525a900fdd80d50c4a64332237
dheeparaj <102752244+dheeparaj@users.noreply.github.com>
2025-08-12 13:57:43 -0700

    TLS Support to OpenSSL workload - users/vdeepar/tls update (#550)
    * basic framework for client-server with openssl
    
    * working version of client server model in VC
    
    * metric parser code added, tested one hour experiment
    
    * metric parser for openssl tls
    
    * update server key and cert path locations in the command
    
    * additional metrics added for TlsOpenSsl
    
    * Copy tlsreource files to openssl binary path
    
    * rename OpensslTls classes with TlsPrefix
    
    * src/VirtualClient/VirtualClient.Actions.UnitTests/OpenSSL/TlsOpenSslClientExecutorTests.cs
    
    * unit tests for TlsClient added
    
    * revert build-packages change
    
    * update Compete-Openssl-Tls profile
    
    ---------
    
    Co-authored-by: Sai Bulusu <sai.bulusu@gmail.com>

---
+++ src/VirtualClient/VirtualClient.Actions/OpenSSL/TlsOpenSslClientExecutor.cs
@@ -0,0 +1,344 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Polly;
+    using VirtualClient;
+    using VirtualClient.Common;
+    using VirtualClient.Common.Contracts;
+    using VirtualClient.Common.Extensions;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// Executes the OpenSSL TLS client workload. Inherits from OpenSslExecutor.
+    /// </summary>
+    public class TlsOpenSslClientExecutor : OpenSslExecutor
+    {
+        private readonly object lockObject = new object();
+        private ISystemManagement systemManagement;
+
+        /// <summary>
+        /// Constructor for the OpenSSL client executor.
+        /// </summary>
+        /// <param name="dependencies">Provides required dependencies to the component.</param>
+        /// <param name="parameters">Parameters defined in the profile or supplied on the command line.</param>
+        public TlsOpenSslClientExecutor(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters = null)
+            : base(dependencies, parameters)
+        {
+            this.ClientFlowRetryPolicy = Policy.Handle<Exception>(exc => !(exc is OperationCanceledException))
+                .WaitAndRetryAsync(3, (retries) => TimeSpan.FromSeconds(retries * 2));
+
+            this.ClientRetryPolicy = Policy.Handle<Exception>(exc => !(exc is OperationCanceledException))
+                .WaitAndRetryAsync(3, (retries) => TimeSpan.FromSeconds(retries));
+
+            this.PollingTimeout = TimeSpan.FromMinutes(40);
+
+            this.ApiClientManager = dependencies.GetService<IApiClientManager>();
+            this.systemManagement = dependencies.GetService<ISystemManagement>();
+        }
+
+        /// <summary>
+        /// Parameter defines the number of client instances to execute against
+        /// each server instance. Default = # of logical cores/vCPUs on system.
+        /// </summary>
+        public int ClientInstances
+        {
+            get
+            {
+                return this.Parameters.GetValue<int>(nameof(this.ClientInstances), 1);
+            }
+        }
+
+        /// <summary>
+        /// gets the OpenSSL server port used for communication.
+        /// </summary>
+        public int ServerPort
+        {
+            get
+            {
+                return this.Parameters.GetValue<int>(nameof(this.ServerPort));
+            }
+        }
+
+        /// <summary>
+        /// Parameter defines true/false whether the action is meant to warm up the server.
+        /// We do not capture metrics on warm up operations.
+        /// </summary>
+        public bool WarmUp
+        {
+            get
+            {
+                return this.Parameters.GetValue<bool>(nameof(this.WarmUp), false);
+            }
+        }
+
+        /// <summary>
+        /// Client used to communicate with the hosted instance of the
+        /// Virtual Client API at server side.
+        /// </summary>
+        protected IApiClient ServerApiClient { get; set; }
+
+        /// <summary>
+        /// Provides the ability to create API clients for interacting with local as well as remote instances
+        /// of the Virtual Client API service.
+        /// </summary>
+        protected IApiClientManager ApiClientManager { get; }
+
+        /// <summary>
+        /// The retry policy to apply to the client-side execution workflow.
+        /// </summary>
+        protected IAsyncPolicy ClientFlowRetryPolicy { get; set; }
+
+        /// <summary>
+        /// The retry policy to apply to each client workload instance when trying to startup
+        /// against a target server.
+        /// </summary>
+        protected IAsyncPolicy ClientRetryPolicy { get; set; }
+
+        /// <summary>
+        /// True/false whether the Redis server instance has been warmed up.
+        /// </summary>
+        protected bool IsServerWarmedUp { get; set; }
+
+        /// <summary>
+        /// The timespan at which the client will poll the server for responses before
+        /// timing out.
+        /// </summary>
+        protected TimeSpan PollingTimeout { get; set; }
+
+        /// <summary>
+        /// Client used to communicate with the locally hosted instance of the
+        /// Virtual Client API.
+        /// </summary>
+        protected IApiClient ApiClient
+        {
+            get
+            {
+                return this.ServerApiClient;
+            }
+        }
+
+        /// <summary>
+        /// Executes  client side.
+        /// </summary>
+        protected override async Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            if (!this.WarmUp || !this.IsServerWarmedUp)
+            {
+                IPAddress ipAddress;
+                List<Task> clientWorkloadTasks = new List<Task>();
+
+                if (this.IsMultiRoleLayout())
+                {
+                    IEnumerable<ClientInstance> targetServers = this.GetLayoutClientInstances(ClientRole.Server);
+                    foreach (ClientInstance server in targetServers)
+                    {
+                        // Reliability/Recovery:
+                        // The pattern here is to allow for any steps within the workflow to fail and to simply start the entire workflow
+                        // over again.
+                        clientWorkloadTasks.Add(this.ClientFlowRetryPolicy.ExecuteAsync(async () =>
+                        {
+                            if (!cancellationToken.IsCancellationRequested)
+                            {
+                                IApiClient serverApiClient = this.ApiClientManager.GetOrCreateApiClient(server.Name, server);
+
+                                // 1) Confirm server is online.
+                                // ===========================================================================
+                                this.Logger.LogTraceMessage("Synchronization: Poll server API for heartbeat...");
+                                await serverApiClient.PollForHeartbeatAsync(this.PollingTimeout, cancellationToken);
+
+                                // 2) Confirm the server-side application is online.
+                                // ===========================================================================
+                                this.Logger.LogTraceMessage("Synchronization: Poll server for online signal...");
+                                await serverApiClient.PollForServerOnlineAsync(TimeSpan.FromSeconds(30), cancellationToken);
+
+                                this.Logger.LogTraceMessage("Synchronization: Server online signal confirmed...");
+                                this.Logger.LogTraceMessage("Synchronization: Start client workload...");
+
+                                // 3) Get Parameters required.
+                                State serverState = await this.GetServerStateAsync(serverApiClient, cancellationToken);
+
+                                // 4) Execute the client workload.
+                                // ===========================================================================
+                                ipAddress = IPAddress.Parse(server.IPAddress);
+
+                                await this.ExecuteWorkloadsAsync(ipAddress, telemetryContext, cancellationToken);
+                            }
+                        }));
+                    }
+                }
+                else
+                {
+                    ipAddress = IPAddress.Loopback;
+                    clientWorkloadTasks.Add(this.ClientFlowRetryPolicy.ExecuteAsync(async () =>
+                    {
+                        if (!cancellationToken.IsCancellationRequested)
+                        {
+                            State serverState = await this.GetServerStateAsync(this.ServerApiClient, cancellationToken);
+                            await this.ExecuteWorkloadsAsync(ipAddress, telemetryContext, cancellationToken);
+                        }
+                    }));
+                }
+
+                await Task.WhenAll(clientWorkloadTasks);
+
+                if (this.WarmUp)
+                {
+                    this.IsServerWarmedUp = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes the environment and dependencies for client of redis Benchmark workload.
+        /// </summary>
+        /// <param name="telemetryContext">Provides context information that will be captured with telemetry events.</param>
+        /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
+        /// <returns></returns>
+        protected override async Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            await base.InitializeAsync(telemetryContext, cancellationToken);
+
+            if (this.IsMultiRoleLayout())
+            {
+                ClientInstance clientInstance = this.GetLayoutClientInstance();
+                string layoutIPAddress = clientInstance.IPAddress;
+
+                this.ThrowIfLayoutClientIPAddressNotFound(layoutIPAddress);
+                this.ThrowIfRoleNotSupported(clientInstance.Role);
+            }
+
+            this.InitializeApiClients();
+        }
+
+        /// <summary>
+        /// Initializes API client.
+        /// </summary>
+        private void InitializeApiClients()
+        {
+            IApiClientManager clientManager = this.Dependencies.GetService<IApiClientManager>();
+            bool isSingleVM = !this.IsMultiRoleLayout();
+
+            if (isSingleVM)
+            {
+                this.ServerApiClient = clientManager.GetOrCreateApiClient(IPAddress.Loopback.ToString(), IPAddress.Loopback);
+            }
+            else
+            {
+                ClientInstance serverInstance = this.GetLayoutClientInstances(ClientRole.Server).First();
+                IPAddress.TryParse(serverInstance.IPAddress, out IPAddress serverIPAddress);
+
+                this.ServerApiClient = clientManager.GetOrCreateApiClient(serverIPAddress.ToString(), serverIPAddress);
+                this.RegisterToSendExitNotifications($"{this.TypeName}.ExitNotification", this.ServerApiClient);
+            }
+        }
+
+        private Task ExecuteWorkloadsAsync(IPAddress serverIPAddress, EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            string commandArguments = this.Parameters.GetValue<string>(nameof(this.CommandArguments));
+            EventContext relatedContext = telemetryContext.Clone()
+                .AddContext("executable", this.ExecutablePath)
+                .AddContext("commandArguments", commandArguments);
+            // ex: command
+            // s_time -connect :{ServerPort} -www /test_1k.html -time {Duration.TotalSeconds} -ciphersuites TLS_AES_256_GCM_SHA384
+            // insert IP address before port at index 16
+            string fullCommand = commandArguments.Insert(16, serverIPAddress.ToString());
+
+            return this.Logger.LogMessageAsync($"{nameof(TlsOpenSslClientExecutor)}.ExecuteOpenSSL_Client_Workload", relatedContext.Clone(), async () =>
+            {
+                using (BackgroundOperations profiling = BackgroundOperations.BeginProfiling(this, cancellationToken))
+                {
+                    using (IProcessProxy process = this.systemManagement.ProcessManager.CreateProcess(this.ExecutablePath, fullCommand))
+                    {
+                        this.SetEnvironmentVariables(process);
+                        this.CleanupTasks.Add(() => process.SafeKill());
+
+                        try
+                        {
+                            await process.StartAndWaitAsync(cancellationToken).ConfigureAwait();
+
+                            if (!cancellationToken.IsCancellationRequested)
+                            {
+                                await this.LogProcessDetailsAsync(process, telemetryContext, "OpenSSLClient", logToFile: true);
+
+                                process.ThrowIfWorkloadFailed();
+                                this.CaptureMetrics(process, fullCommand, telemetryContext, cancellationToken);
+                            }
+                        }
+                        finally
+                        {
+                            if (!process.HasExited)
+                            {
+                                process.Kill();
+                            }
+                        }
+                    }
+
+                }
+            });
+        }
+
+        private async Task<State> GetServerStateAsync(IApiClient serverApiClient, CancellationToken cancellationToken)
+        {
+            Item<State> state = await serverApiClient.GetStateAsync<State>(
+                nameof(State),
+                cancellationToken);
+
+            if (state == null)
+            {
+                throw new WorkloadException(
+                    $"Expected server state information missing. The openssl tls server did not return state indicating the details for the server(s) running.",
+                    ErrorReason.WorkloadUnexpectedAnomaly);
+            }
+
+            return state.Definition;
+        }
+
+        private void CaptureMetrics(IProcessProxy workloadProcess, string commandArguments, EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            if (workloadProcess.ExitCode == 0)
+            {
+                try
+                {
+                    // Retrieve OpenSSL version
+                    // await this.GetOpenSslVersionAsync(workloadProcess.FullCommand(), cancellationToken);
+
+                    this.MetadataContract.Apply(telemetryContext);
+
+                    OpenSslTlsMetricsParser resultsParser = new OpenSslTlsMetricsParser(workloadProcess.StandardOutput.ToString(), commandArguments);
+                    IList<Metric> metrics = resultsParser.Parse();
+
+                    this.Logger.LogMetrics(
+                        "OpenSSL_tls_client",
+                        this.MetricScenario ?? this.Scenario,
+                        workloadProcess.StartTime,
+                        workloadProcess.ExitTime,
+                        metrics,
+                        null,
+                        commandArguments,
+                        this.Tags,
+                        telemetryContext);
+
+                    metrics.LogConsole(this.Scenario, "OpenSSL_tls_client");
+                }
+                catch (SchemaException exc)
+                {
+                    EventContext relatedContext = telemetryContext.Clone()
+                        .AddError(exc);
+
+                    this.Logger.LogMessage($"{nameof(TlsOpenSslClientExecutor)}.WorkloadOutputParsingFailed", LogLevel.Warning, relatedContext);
+                }
+            }
+        }
+    }
+}
