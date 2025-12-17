acfbbbeb609b78797c04228b910e4e5961bc8839
nchapagain001 <165215502+nchapagain001@users.noreply.github.com>
2025-12-17 14:58:59 -0800

    Adding token

---
+++ src/VirtualClient/VirtualClient.Main/GetAccessTokenCommand.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// Command that executes a profile to acquire an access token for an Azure Key Vault.
+    /// </summary>
+    internal class GetAccessTokenCommand : ExecuteProfileCommand
+    {
+        /// <summary>
+        /// Executes the access token acquisition operations using the configured profile.
+        /// </summary>
+        /// <param name="args">The arguments provided to the application on the command line.</param>
+        /// <param name="cancellationTokenSource">Provides a token that can be used to cancel the command operations.</param>
+        /// <returns>The exit code for the command operations.</returns>
+        public override Task<int> ExecuteAsync(string[] args, CancellationTokenSource cancellationTokenSource)
+        {
+            this.Timeout = ProfileTiming.OneIteration();
+            this.Profiles = new List<DependencyProfileReference>
+            {
+                new DependencyProfileReference("GET-ACCESS-TOKEN.json")
+            };
+
+            if (this.Parameters == null)
+            {
+                this.Parameters = new Dictionary<string, IConvertible>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            return base.ExecuteAsync(args, cancellationTokenSource);
+        }
+    }
+}
