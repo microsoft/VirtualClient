a9b50ed9740ec5c6ffcba090bb2491d0fdb8fa9c
saibulusu <saibulusu@microsoft.com>
2026-01-27 09:55:04 -0800

    reportable only if the full suite is used for the benchmarks.

--- src/VirtualClient/VirtualClient.Actions/SPECcpu/SpecCpuExecutor.cs
+++ src/VirtualClient/VirtualClient.Actions/SPECcpu/SpecCpuExecutor.cs
@@ -399,6 +399,8 @@ namespace VirtualClient.Actions
 
         private string GetCommandLineArguments()
         {
+            List<string> suites = new List<string> { "intrate", "intspeed", "fprate", "fpspeed" };
+
             // runcpu arguments document: https://www.spec.org/cpu2017/Docs/runcpu.html#strict
             string configurationFile = this.GetConfigurationFileName();
 
@@ -406,7 +408,7 @@ namespace VirtualClient.Actions
 
             // For linux runs we are doing reportable. For windows since not all benchmarks could be run, it will be noreportable.
             // Iterations has to be either 2 or 3 for reportable runs. https://www.spec.org/cpu2017/Docs/config.html#reportable
-            bool reportable = (this.Platform == PlatformID.Unix) && (this.Iterations == 2 || this.Iterations == 3);
+            bool reportable = (suites.Contains(this.Benchmarks.ToLower())) && (this.Iterations == 2 || this.Iterations == 3);
             cmd = reportable ? $"{cmd} --reportable" : $"{cmd} --noreportable";
             cmd = $"{cmd} {this.Benchmarks}";
             return cmd;
