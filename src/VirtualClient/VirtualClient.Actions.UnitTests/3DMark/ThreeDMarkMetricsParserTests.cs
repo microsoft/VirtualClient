6f3e0f2a882fad6ee58edad86a6149f9e0cbf823
Rakeshwar Reddy Kambaiahgari <rkambaiahgar@microsoft.com>
2026-02-19 20:07:08 -0800

    Reverted changes

--- src/VirtualClient/VirtualClient.Actions.UnitTests/3DMark/ThreeDMarkMetricsParserTests.cs
+++ src/VirtualClient/VirtualClient.Actions.UnitTests/3DMark/ThreeDMarkMetricsParserTests.cs
@@ -3,75 +3,62 @@
 
 namespace VirtualClient.Actions
 {
-    using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
+    using System.Diagnostics;
     using System.IO;
     using System.Linq;
-    using System.Reflection.Metadata;
+    using System.Reflection;
     using System.Text;
-    using System.Text.RegularExpressions;
-    using System.Xml.Linq;
-    using System.Xml.Serialization;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using VirtualClient.Common;
     using VirtualClient.Contracts;
 
-    /// <summary>
-    /// Parser for the NTttcp workload.
-    /// </summary>
-    public class ThreeDMarkMetricsParser : MetricsParser
+    [TestFixture]
+    [Category("Unit")]
+    public class ThreeDMarkMetricsParserTests
     {
-        /// <summary>
-        /// Parser for the 3DMark workload
-        /// </summary>
-        /// <param name="rawText">The raw text from the 3DMark export process.</param>
-        /// <param name="definition">The 3dmark definition.</param>
-        public ThreeDMarkMetricsParser(string rawText, string definition)
-            : base(rawText)
+        [Test]
+        public void ThreeDMarkMetricsParserTestsCorrectly_ScenarioTSGT1()
         {
-            this.Defintion = definition;
+            string workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string outputPath = Path.Combine(workingDirectory, "Examples", "3DMark", "result_tsgt1.xml");
+            string rawText = File.ReadAllText(outputPath);
+
+            ThreeDMarkMetricsParser testParser = new ThreeDMarkMetricsParser(rawText, "custom_TSGT1.3dmdef");
+            IList<Metric> metrics = testParser.Parse();
+
+            Assert.AreEqual(1, metrics.Count);
+            MetricAssert.Exists(metrics, "timespy.graphics.1 [fps]", 59.65, "fps");
         }
 
-        private string Defintion { get; set; }
-
-        /// <inheritdoc/>
-        public override IList<Metric> Parse()
+        [Test]
+        public void ThreeDMarkMetricsParserTestsCorrectly_ScenarioTSGT2()
         {
-            IList<Metric> metrics = new List<Metric>();
-            try
-            {
-                if (this.Defintion == "custom_TSGT1.3dmdef")
-                {
-                    metrics.Add(new Metric("timespy.graphics.1 [fps]", this.ParseXMLTag("TimeSpyPerformanceGraphicsTest1"), "fps", MetricRelativity.HigherIsBetter, verbosity: 1));
-                }
-                else if (this.Defintion == "custom_TSGT2.3dmdef")
-                {
-                    metrics.Add(new Metric("timespy.graphics.2 [fps]", this.ParseXMLTag("TimeSpyPerformanceGraphicsTest2"), "fps", MetricRelativity.HigherIsBetter, verbosity: 1));
-                }
-                else if (this.Defintion == "custom_TSCT.3dmdef")
-                {
-                    metrics.Add(new Metric("timespy.cpu [fps]", this.ParseXMLTag("TimeSpyPerformanceCpuSection2"), "fps", MetricRelativity.HigherIsBetter, verbosity: 1));
-                }
-            }
-            catch (Exception exc)
-            {
-                throw new WorkloadException($"Results not found. The workload '3DMark' did not produce any valid results.", exc, ErrorReason.WorkloadFailed);
-            }
+            string workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string outputPath = Path.Combine(workingDirectory, "Examples", "3DMark", "result_tsgt2.xml");
+            string rawText = File.ReadAllText(outputPath);
 
-            return metrics;
+            ThreeDMarkMetricsParser testParser = new ThreeDMarkMetricsParser(rawText, "custom_TSGT2.3dmdef");
+            IList<Metric> metrics = testParser.Parse();
+
+            Assert.AreEqual(1, metrics.Count);
+            MetricAssert.Exists(metrics, "timespy.graphics.2 [fps]", 58.10, "fps");
         }
 
-        /// <summary>
-        /// Gets the value sandwiched between the given XML tags
-        /// </summary>
-        /// <param name="tagName"></param>
-        /// <returns></returns>
-        private double ParseXMLTag(string tagName)
+        [Test]
+        public void ThreeDMarkMetricsParserTestsCorrectly_ScenarioTSCT()
         {
-            string pattern = $"<{tagName}.*>((.|\\n)*?)<\\/{tagName}>";
-            Match m = Regex.Match(this.RawText, pattern);
-            XElement tag = XElement.Parse(m.Value);
-            double val = double.Parse(tag.Value);
-            return val;
+            string workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string outputPath = Path.Combine(workingDirectory, "Examples", "3DMark", "result_tsct.xml");
+            string rawText = File.ReadAllText(outputPath);
+
+            ThreeDMarkMetricsParser testParser = new ThreeDMarkMetricsParser(rawText, "custom_TSCT.3dmdef");
+            IList<Metric> metrics = testParser.Parse();
+
+            Assert.AreEqual(1, metrics.Count);
+            MetricAssert.Exists(metrics, "timespy.cpu [fps]", 28.50, "fps");
         }
+
     }
 }
