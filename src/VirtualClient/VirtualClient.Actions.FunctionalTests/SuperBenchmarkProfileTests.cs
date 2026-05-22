4f8b18ddf88bcefc5cb56a6ffb91f708217938b3
saibulusu <saibulusu@microsoft.com>
2026-05-22 16:00:37 -0700

    Fixing profile test for superbench with a100 setup

--- src/VirtualClient/VirtualClient.Actions.FunctionalTests/SuperBenchmarkProfileTests.cs
+++ src/VirtualClient/VirtualClient.Actions.FunctionalTests/SuperBenchmarkProfileTests.cs
@@ -113,7 +113,7 @@ namespace VirtualClient.Actions
         private IEnumerable<string> GetProfileExpectedCommands(PlatformID platform)
         {
             string setupCommand = "curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey "
-            + "| sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \\\n  "
+            + "| sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \\\n  "
             + "&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \\\n "
             + "   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \\\n  "
             + "  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list";
