384e2f1da382973f0a555e38be4e02acf22c5019
Sai Bulusu <sai.bulusu@gmail.com>
2026-06-01 15:20:25 -0700

    elastic search migration to open-source (#723)
    * Copying over files for elastic search/rally from internal.
    
    * documentation
    
    * upversion
    
    ---------
    
    Co-authored-by: saibulusu <saibulusu@microsoft.com>

---
+++ src/VirtualClient/VirtualClient.Actions/ElasticsearchRally/ElasticsearchRallyMetricsParser.cs
@@ -0,0 +1,307 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using VirtualClient;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// Elasticsearch Rally metrics parser that parses the raw text output from Elasticsearch Rally and converts it into a list of <see cref="Metric"/> objects.
+    /// </summary>
+    public class ElasticsearchRallyMetricsParser : MetricsParser
+    {
+        /// <summary>
+        /// Constructor for <see cref="ElasticsearchRallyMetricsParser"/>
+        /// </summary>
+        /// <param name="reportContents">Text to be parsed.</param>
+        /// <param name="metadata">Metadata associated with the metrics.</param>
+        /// <param name="rallyCollectAllMetrics">Indicates whether to collect all metrics.</param>
+        public ElasticsearchRallyMetricsParser(string reportContents, Dictionary<string, IConvertible> metadata, bool rallyCollectAllMetrics)
+            : base(reportContents)
+        {
+            if (string.IsNullOrEmpty(reportContents))
+            {
+                throw new ArgumentNullException(nameof(reportContents), "Report contents cannot be null.");
+            }
+
+            if (metadata == null)
+            {
+                throw new ArgumentNullException(nameof(metadata));
+            }
+            
+            this.ReportLines = reportContents.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in metadata)
+            {
+                this.Metadata[item.Key] = item.Value;
+            }
+
+            this.RallyCollectAllMetrics = rallyCollectAllMetrics;
+        }
+
+        /// <summary>
+        /// Indicates whether to collect all metrics.
+        /// </summary>
+        public bool RallyCollectAllMetrics { get; private set; }
+
+        /// <summary>
+        /// Allows the user to describe different levels of priority/verbosity to a set of metrics that can 
+        /// be used for queries/filtering. Lower values indicate higher priority. For example, metrics considered 
+        /// to be the most critical for decision making would be set with verbosity = 1 (Critical).
+        /// </summary>
+        public int MetricsVerbosity
+        {
+            get
+            {
+                if (this.Metadata.TryGetValue(nameof(this.MetricsVerbosity), out IConvertible verbosityValue) && int.TryParse(verbosityValue.ToString(), out int verbosity))
+                {
+                    return verbosity;
+                }
+
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// The lines of the report to be parsed.
+        /// </summary>
+        public string[] ReportLines { get; }
+
+        /// <summary>
+        /// Parses the raw text output from Elasticsearch Rally and converts it into a list of <see cref="Metric"/> objects.
+        /// </summary>
+        /// <returns>A list of <see cref="Metric"/> objects.</returns>
+        public override IList<Metric> Parse()
+        {
+            return Read(this.ReportLines, this.Metadata, this.RallyCollectAllMetrics, this.MetricsVerbosity);
+        }
+
+        private static IList<Metric> Read(
+            string[] reportLines,
+            IDictionary<string, IConvertible> metadata,
+            bool collectAllMetrics,
+            int metricsVerbosity)
+        {
+            if (reportLines == null)
+            {
+                throw new ArgumentNullException(nameof(reportLines), "Report lines cannot be null.");
+            }
+
+            if (metadata == null)
+            {
+                throw new ArgumentNullException(nameof(metadata), "Metadata cannot be null.");
+            }
+
+            IList<Metric> metrics = new List<Metric>();
+
+            foreach (string line in reportLines)
+            {
+                // Metric,Task,Value,Unit
+                string[] cols = line.Split(',');
+
+                if (cols.Length != 4 || !double.TryParse(cols[2], out double value))
+                {
+                    continue;
+                }
+
+                string metricName = cols[0].ToLower();
+                string taskName = cols[1].ToLower();
+
+                // only relevant metrics will be collected
+                if (!collectAllMetrics &&
+                    !IsRelevantMetric(metricName, taskName))
+                {
+                    continue;
+                }
+
+                string unit = cols[3];
+                bool isTimeUnit = unit == "s" || unit == "ms" || unit == "min";
+
+                if (unit.Length == 0 && metricName.Contains($" {MetricNames.Count}"))
+                {
+                    unit = MetricNames.Count;
+                }
+
+                MetricRelativity relativity = MetricRelativity.Undefined;
+
+                if (metricName.EndsWith(MetricNames.Throughput))
+                {
+                    relativity = MetricRelativity.HigherIsBetter;
+                }
+                else if (
+                    metricName.EndsWith(MetricNames.Latency) ||
+                    metricName.EndsWith(MetricNames.ServiceTime) ||
+                    metricName.EndsWith(MetricNames.Rate))
+                {
+                    relativity = MetricRelativity.LowerIsBetter;
+                }
+
+                if (relativity == MetricRelativity.Undefined)
+                {
+                    if (isTimeUnit)
+                    {
+                        relativity = MetricRelativity.LowerIsBetter;
+                    }
+                }
+
+                metricName = TransformMetricName(metricName);
+
+                if (taskName.Length > 0)
+                {
+                    metricName = $"{taskName} {metricName}";
+                }
+
+                metricName = metricName.Replace(' ', '-');
+
+                Metric metric = new Metric(
+                    name: metricName,
+                    value: value,
+                    unit: unit,
+                    relativity: relativity,
+                    metadata: metadata);
+
+                if (!CheckVerbosity(metric, metricsVerbosity) && !collectAllMetrics)
+                {
+                    continue;
+                }
+
+                metrics.Add(metric);
+            }
+
+            return metrics;
+        }
+
+        /// <summary>
+        /// Verbosity levels define a convention for organizing metrics by importance:
+        /// - 1 (Standard/Critical): Most important metrics for decision making - bandwidth, throughput, IOPS, key latency percentiles (p50, p99)
+        /// - 2 (Detailed): Additional detailed metrics - supplementary percentiles (p70, p90, p95, p99.9)
+        /// - 3 (Reserved): Reserved for future expansion
+        /// - 4 (Reserved): Reserved for future expansion
+        /// - 5 (Verbose): All diagnostic/internal metrics - histogram buckets, standard deviations, byte counts, I/O counts
+        /// https://github.com/microsoft/VirtualClient/blob/f1d5410ac2c1cd1acfa6a0901af79cbef1abe9df/src/VirtualClient/VirtualClient.Contracts/Metric.cs#L163
+        /// </summary>
+        /// <param name="metric"></param>
+        /// <param name="metricsVerbosity"></param>
+        /// <returns></returns>
+        private static bool CheckVerbosity(Metric metric, int metricsVerbosity)
+        {
+            if (metric.Name.EndsWith("P100") ||
+                metric.Name.EndsWith("Mean") || metric.Name.EndsWith("Median") || (metric.Name.Contains("latency") || metric.Name.Contains("throughput")))
+            {
+                metric.Verbosity = 1; // Critical
+            }
+            else if (metric.Name.EndsWith("P50") || metric.Name.EndsWith("P90") || metric.Name.EndsWith("P99"))
+            {
+                metric.Verbosity = 2; // Detailed
+            }
+            else
+            {
+                metric.Verbosity = 5; // Verbose
+            }
+            
+            return metric.Verbosity <= metricsVerbosity;
+        }
+
+        private static bool SwapPrefix(string metricName, string oldPrefix, string newPrefix, out string newMetricName)
+        {
+            if (metricName.StartsWith(oldPrefix))
+            {
+                newMetricName = $"{metricName.Substring(oldPrefix.Length).Trim()} {newPrefix}";
+                return true;
+            }
+
+            newMetricName = metricName;
+            return false;
+        }
+
+        private static bool SwapPercentileFormat(string metricName, out string newMetricName)
+        {
+            // "100th percentile latency" => "latency P100"
+
+            var match = System.Text.RegularExpressions.Regex.Match(metricName, @"^(\d+)th (percentile) (.+)");
+            if (match.Success)
+            {
+                newMetricName = $"{match.Groups[3].Value} P{match.Groups[1].Value}";
+                return true;
+            }
+
+            newMetricName = metricName;
+            return false;
+        }
+
+        private static string TransformMetricName(string metricName)
+        {
+            if (SwapPrefix(metricName, "median", "Median", out string newMetricName))
+            {
+                return newMetricName;
+            }
+            else if (SwapPrefix(metricName, "mean", "Mean", out newMetricName))
+            {
+                return newMetricName;
+            }
+            else if (SwapPercentileFormat(metricName, out newMetricName))
+            {
+                return newMetricName;
+            }
+
+            return metricName;
+        }
+
+        private static bool IsRelevantMetric(string metricName, string taskName)
+        {
+            if (string.IsNullOrEmpty(taskName))
+            {
+                // summary metrics
+                return
+                    new string[]
+                    {
+                        "median cumulative indexing time across primary shards",
+                        "median cumulative merge time across primary shards",
+                        "median cumulative refresh time across primary shards",
+                        "median cumulative flush time across primary shards",
+                        "total young gen gc time",
+                        "dataset size",
+                        "translog size",
+                        "segment count",
+                    }.Contains(metricName);
+            }
+            else
+            {
+                // task metrics
+                return
+                    new string[]
+                    {
+                        "mean throughput",
+                        "median throughput",
+                        "50th percentile latency",
+                        "90th percentile latency",
+                        "99th percentile latency",
+                        "100th percentile latency",
+                        "50th percentile service time",
+                        "90th percentile service time",
+                        "99th percentile service time",
+                        "100th percentile service time",
+                        "error rate",
+                    }.Contains(metricName);
+            }
+        }
+
+        private struct MetricNames
+        {
+            public const string Count = "count";
+            public const string Latency = "latency";
+            public const string Median = "median";
+            public const string ServiceTime = "service time";
+            public const string P50 = "50th";
+            public const string P90 = "90th";
+            public const string P99 = "99th";
+            public const string P100 = "100th";
+            public const string Rate = "rate";
+            public const string Throughput = "throughput";
+        }
+    }
+}
---
+++ src/VirtualClient/VirtualClient.Actions/ElasticsearchRally/ElasticsearchRallyServerExecutor.cs
@@ -0,0 +1,263 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace VirtualClient.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using VirtualClient;
+    using VirtualClient.Common.Telemetry;
+    using VirtualClient.Contracts;
+
+    /// <summary>
+    /// The Elasticsearch Rally Server workload executor.
+    /// </summary>
+    public class ElasticsearchRallyServerExecutor : ElasticsearchRallyExecutor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ElasticsearchRallyServerExecutor"/> class.
+        /// </summary>
+        /// <param name="dependencies">An enumeration of dependencies that can be used for dependency injection.</param>
+        /// <param name="parameters">An enumeration of key-value pairs that can control the execution of the component.</param>
+        public ElasticsearchRallyServerExecutor(IServiceCollection dependencies, IDictionary<string, IConvertible> parameters = null)
+            : base(dependencies, parameters)
+        {
+        }
+        
+        /// <summary>
+        /// The timeout duration (in milliseconds) to wait for Elasticsearch to become available after starting.
+        /// </summary>
+        protected int WaitForElasticsearchAvailabilityTimeout { get; set; } = 30000;
+
+        /// <summary>
+        /// Initializes the environment for execution of the Rally workload.
+        /// </summary>
+        protected override async Task InitializeAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            await base.InitializeAsync(telemetryContext, cancellationToken)
+                .ConfigureAwait(false);
+
+            await this.Logger.LogMessageAsync($"{this.TypeName}.ConfigureServer", telemetryContext.Clone(), async () =>
+            {
+                ElasticsearchRallyState state = await this.StateManager.GetStateAsync<ElasticsearchRallyState>(nameof(ElasticsearchRallyState), cancellationToken)
+                    ?? new ElasticsearchRallyState();
+
+                if (!state.ElasticsearchStarted)
+                {
+                    await this.StartElasticsearch(telemetryContext, cancellationToken);
+
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        state.ElasticsearchStarted = true;
+                        await this.StateManager.SaveStateAsync<ElasticsearchRallyState>(nameof(ElasticsearchRallyState), state, cancellationToken);
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// Executes server side of workload.
+        /// </summary>
+        /// <param name="telemetryContext">Provides context information that will be captured with telemetry events.</param>
+        /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
+        protected override Task ExecuteAsync(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            return this.Logger.LogMessageAsync($"{nameof(ElasticsearchRallyServerExecutor)}.ExecuteServer", telemetryContext, async () =>
+            {
+                try
+                {
+                    this.SetServerOnline(true);
+
+                    if (this.IsMultiRoleLayout())
+                    {
+                        using (BackgroundOperations profiling = BackgroundOperations.BeginProfiling(this, cancellationToken))
+                        {
+                            await this.WaitAsync(cancellationToken);
+                        }
+                    }
+                }
+                finally
+                {
+                    this.SetServerOnline(false);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Write all text to a file.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <param name="content">The content to write to the file.</param>
+        protected virtual void WriteAllText(string path, string content)
+        {
+            File.WriteAllText(path, content);
+        }
+
+        /// <summary>
+        /// Read all text from a file.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <returns>The content of the file.</returns>
+        protected virtual string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        private async Task StartElasticsearch(EventContext telemetryContext, CancellationToken cancellationToken)
+        {
+            string mountPoint = await this.GetDataDirectoryAsync(cancellationToken);
+
+            if (this.Platform == PlatformID.Unix)
+            {
+                this.StartElasticsearchLinux(telemetryContext, cancellationToken, mountPoint);
+            }
+            else
+            {
+                throw new NotSupportedException($"The {this.TypeName} does not support execution on {this.Platform} operating system.");
+            }
+        }
+
+        private void StartElasticsearchLinux(EventContext telemetryContext, CancellationToken cancellationToken, string mountPoint)
+        {
+            string scriptsDirectory = this.PlatformSpecifics.GetScriptPath(this.PackageName.ToLower());
+            int port = this.Port;
+
+            telemetryContext.AddContext(nameof(mountPoint), mountPoint);
+            telemetryContext.AddContext(nameof(scriptsDirectory), scriptsDirectory);
+            telemetryContext.AddContext(nameof(port), port);
+
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "SetVmMaxMapCount", "sysctl -w vm.max_map_count=262144");
+
+            // make the change persistent
+            this.RunCommandScript(telemetryContext, cancellationToken, "VmMaxMapCountPersist", "echo \"vm.max_map_count=262144\" | sudo tee /etc/sysctl.d/99-elasticsearch.conf");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "VmMaxMapCountSysCtl", "sysctl --system");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "VmMaxMapCountVerify", "sysctl vm.max_map_count");
+
+            // LimitMEMLOCKinfinity
+            this.RunCommandScript(telemetryContext, cancellationToken, "LimitMEMLOCKinfinityMkdir", "sudo mkdir -p /etc/systemd/system/elasticsearch.service.d");
+            this.RunCommandScript(telemetryContext, cancellationToken, "LimitMEMLOCKinfinityPersist", "printf \"[Service]\nLimitMEMLOCK=infinity\nLimitMEMLOCKSoft=infinity\n\" | sudo tee /etc/systemd/system/elasticsearch.service.d/override.conf");
+
+            // download and install elasticsearch
+            this.InstallElasticsearchLinux(telemetryContext, cancellationToken, scriptsDirectory);
+
+            (string rootPath, string dataPath, string logPath) = GetElasticsearchPathsLinux(mountPoint);
+
+            if (!string.IsNullOrEmpty(mountPoint))
+            {
+                // create data and log directories
+                this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchRootPathMkdir", $"mkdir -p {rootPath}");
+
+                // check if mounted filesystem is read‑only
+                this.RunCommandScript(telemetryContext, cancellationToken, "CheckMountedFileSystem", $"mount | grep {mountPoint}");
+
+                // set ownership
+                this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchRepoChown", $"chmod -R 777 {rootPath}", true);
+            }
+
+            // create elasticsearch.yml
+            string elasticsearchPathYml = this.CreateElasticsearchYml(telemetryContext, scriptsDirectory, port, dataPath, logPath);
+
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchYmlCopy", $"cp {elasticsearchPathYml} /etc/elasticsearch/elasticsearch.yml");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchYml", $"tail -n 10000 /etc/elasticsearch/elasticsearch.yml");
+
+            // set limits.conf
+            string limitsConfPath = this.PlatformSpecifics.Combine(scriptsDirectory, "limits.ini");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchLimitsCopy", $"cp {limitsConfPath} /etc/security/limits.conf");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchLimits", $"tail -n 10000 /etc/security/limits.conf");
+
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchRemoveKeystore", "/usr/share/elasticsearch/bin/elasticsearch-keystore remove xpack.security.transport.ssl.keystore.secure_password");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchRemoveTruestore", "/usr/share/elasticsearch/bin/elasticsearch-keystore remove xpack.security.transport.ssl.truststore.secure_password");
+
+            // run elasticsearch
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchDaemonReexec", "systemctl daemon-reexec");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchEnable", "systemctl enable elasticsearch");
+            bool ok = this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchStart", "systemctl start elasticsearch.service");
+            Thread.Sleep(30000); // wait for elasticsearch to start
+
+            if (!ok)
+            {
+                this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchJournal", "journalctl -xeu elasticsearch.service");
+
+                throw new WorkloadException(
+                    $"Elasticsearch failed to start.",
+                    ErrorReason.WorkloadUnexpectedAnomaly);
+            }
+
+            // verify elasticsearch is running
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchStatus", "systemctl status elasticsearch.service");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchSocket", "ss -lnt");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchUrlCall", $"curl localhost:{port}");
+        }
+
+        private void InstallElasticsearchLinux(EventContext telemetryContext, CancellationToken cancellationToken, string scriptsDirectory)
+        {
+            // VirtualClient profile script using LinuxPackageInstallation is throwing a reachability error from Juno deployment: Unable to locate package elasticsearch
+            // manual installation is not working via wget in VMs deployed by Juno, using Apt instead.
+
+            string elasticsearchMajorVersionKey = $"{this.ElasticsearchVersion.Substring(0, 1)}.x";
+            telemetryContext.AddContext(nameof(elasticsearchMajorVersionKey), elasticsearchMajorVersionKey);
+
+            // Elasticsearch documentation here https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
+            this.RunCommandScript(telemetryContext, cancellationToken, "ElasticsearchImport", "curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg");
+            this.RunCommandScript(telemetryContext, cancellationToken, "ElasticsearchAdd", $"echo \"deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/{elasticsearchMajorVersionKey}/apt stable main\" | sudo tee /etc/apt/sources.list.d/elastic-{elasticsearchMajorVersionKey}.list");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchUpdate", "apt update");
+            this.RunCommandAsRoot(telemetryContext, cancellationToken, "ElasticsearchInstall", "apt install elasticsearch -y");
+        }
+
+        private static (string root, string data, string log) GetElasticsearchPathsLinux(string mountPoint)
+        {
+            string rootPath;
+            if (mountPoint == null)
+            {
+                rootPath = "/var/elasticsearch";
+            }
+            else
+            {
+                rootPath = Path.Combine(mountPoint, "elasticsearch");
+            }
+
+            return (rootPath, $"{rootPath}/data", $"{rootPath}/log");
+        }
+
+        private string CreateElasticsearchYml(EventContext telemetryContext, string scriptsDirectory, int port, string dataPath, string logPath)
+        {
+            string elasticsearchPath = this.PlatformSpecifics.Combine(scriptsDirectory, "elasticsearch.ini");
+            if (!this.CheckFileExists(elasticsearchPath))
+            {
+                throw new WorkloadException(
+                    $"The Elasticsearch configuration file (yml) could not be found at the expected path: {elasticsearchPath}",
+                    ErrorReason.WorkloadUnexpectedAnomaly);
+            }
+
+            Dictionary<string, string> ymlparameters = new Dictionary<string, string>()
+            {
+                { "port", port.ToString() },
+                { "path.data", dataPath },
+                { "path.logs", logPath },
+            };
+
+            telemetryContext.AddContext($"{this.TypeName}.{nameof(elasticsearchPath)}", elasticsearchPath);
+            this.Logger.LogMessage($"{this.TypeName}.ElasticsearchIniRead", telemetryContext);
+            string elasticsearchYmlContent = this.ReadAllText(elasticsearchPath);
+
+            foreach (KeyValuePair<string, string> p in ymlparameters)
+            {
+                telemetryContext.AddContext($"{this.TypeName}.YmlParameter.{p.Key}", p.Value);
+
+                elasticsearchYmlContent = elasticsearchYmlContent.Replace($"$.parameters.{p.Key}", p.Value);
+            }
+
+            elasticsearchYmlContent = elasticsearchYmlContent.Replace("$.parameters.port", port.ToString());
+
+            string elasticsearchPathYml = elasticsearchPath.Replace(".ini", ".yml");
+            this.Logger.LogMessage($"{this.TypeName}.ElasticsearchYmlWrite", telemetryContext);
+            this.WriteAllText(elasticsearchPathYml, elasticsearchYmlContent);
+
+            return elasticsearchPathYml;
+        }
+    }
+}
